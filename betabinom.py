f2d81c602d3ecf79ea87e123bb6f3d1d3a780aca
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-03 00:29:08 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -12,11 +12,9 @@ st.markdown(""" <style> .font {
 #----------------------------------------------------#
 col1, col2, col3 = st.beta_columns([1,6,1])
 with col1:
-st.write("")
 with col2:
 st.image("Avatar-con-naming-Fliflax.jpg",width=400)
 with col3:
-st.write("")
 #----------------------------------------------------#
 
 st.markdown('<p style="font-family:Consolas; color:#000000; font-size: 50px;"><b>Modelo Beta-binomial</b></p>', unsafe_allow_html=True)
