381f818f784388f714de28b9984959aa83ab9495
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-03 19:00:45 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -43,12 +43,12 @@ with col1:
     A1 = st.number_input("Audiencia acumulada tras 1 inserción", min_value = 1, value = A1_default, step=100, key = "A1")
     # st.write("Valor elegido: {:.0f}".format(A1))
 with col2:
-    A2 = st.number_input("Audiencia acumulada tras 2 inserciones", min_value = 1, max_value = round(A1 * 2), value = A1 * 1, step=100, key = "A2")
+    A2 = st.number_input("Audiencia acumulada tras 2 inserciones", min_value = 1, max_value = round(A1 * 2), value = A1, step=100, key = "A2")
     # st.write("Valor elegido: {:.0f}".format(A2))
 
 col1, col2 = st.columns([5,5])
 with col1:
-    P = st.number_input("Población", min_value = A2, value = A1 * 5, step=100, key = "poblacion")
+    P = st.number_input("Población", min_value = A2, value = A2 * 5, step=100, key = "poblacion")
     # st.write("Valor elegido: {}".format(P))
 with col2:    
     Precio = st.number_input("Precio de una inserción €", min_value = 1, value = 100000, step=100, key = "precio")
