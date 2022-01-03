d0483f0fa8d4dc782d2636de01a63b4031efa6af
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-03 17:42:35 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -16,7 +16,7 @@ st.markdown(""" <style> .font {
     </style> """, unsafe_allow_html=True)
 #----------------------------------------------------#
 col1, col2, col3 = st.columns([3, 5, 0.2])
-col1.image("Avatar-con-naming-Fliflax.jpg", use_column_width=True, width = 500)
+col1.image("Avatar-con-naming-Fliflax.jpg", use_column_width=True, width = 400)
 with col2:
     st.markdown('<div style="text-align:center"><p style="font-family:sans-serif; color:#000000; font-size: 50px;"><b>Modelo<br>Beta-Binomial</b></p></div>', unsafe_allow_html=True)
 #----------------------------------------------------#
