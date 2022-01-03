2e2b67bff404da54bdb8b124b06ef620dcf57df2
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-03 18:24:11 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -1,8 +1,6 @@
-import time
-my_bar = st.progress(0)
-for percent_complete in range(100):
-     time.sleep(0.1)
-     my_bar.progress(percent_complete + 1) 
+with st.spinner('Wait for it...'):
+    time.sleep(5)
+st.success('Done!')
 #----------------------------------------------------#
 #----------------------------------------------------#
 import streamlit as st
