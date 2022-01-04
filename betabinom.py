257b891d7f76a52af9397543c3290142c94f8c3f
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-04 12:22:36 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -174,7 +174,7 @@ st.markdown("""---""")
 # Mostrar la tabla de Pi y Ri:
 # Convierto en index columna de exposiciones, y vuelco en otra tabla porque si no, me genera arror en Matplotlib.
 df1 = df.set_index('exposiciones')
-if st.checkbox("Si deseas ver los primeros 5 valores de Pi y Ri alcanzados, marca la casilla.", False):
+if st.sidebar.checkbox("Si deseas ver los primeros 5 valores de Pi y Ri alcanzados, marca la casilla.", False):
     st.write('###### Tabla 1. Distribución de contactos Pi (y acumulada Ri)')
     st.table(df1.head().style.format("{:,.0f}").set_properties(**{'text-align': 'center'}).set_properties(**{'background-color': '#ffffff'})) 
     st.info("En nuestro Anexo de abajo, puedes ver todos los valores de Pi y Ri.")
