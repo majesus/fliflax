8b75a9737071118d5642fa6f1ca2637ebc85d22c
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-04 21:37:59 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -184,10 +184,8 @@ st.markdown("""---""")
 #df1 = df.set_index('exposiciones')
 df1 = pd.DataFrame(df)
 
-a = alt.Chart(df1).mark_area(opacity=1).encode(
-    x='exposiciones', y='Pi')
-b = alt.Chart(df1).mark_area(opacity=0.6).encode(
-    x='exposiciones', y='Ri')
+a = alt.Chart(df1).mark_area(opacity=1).encode(x='exposiciones', y='Pi')
+b = alt.Chart(df1).mark_area(opacity=0.6).encode(x='exposiciones', y='Ri')
 c = alt.layer(a, b)
 
 if st.checkbox("Si deseas ver la representación gráfica de la distribución de contactos Pi (y acumulada Ri), marca la casilla.", False):
