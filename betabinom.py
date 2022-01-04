c6d129ef2e1f826dee5ca1b113613991aa913f84
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-04 21:41:13 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -243,3 +243,10 @@ if st.checkbox("Si deseas ver la tabla completa de valores de Pi y Ri alcanzados
     st.table(df1.style.format("{:,.0f}").set_properties(**{'text-align': 'center'}).set_properties(**{'background-color': '#ffffff'})) 
     st.write('Parámetros de forma: alfa: ',f"{alphas:,.3f}",'y beta: ',f"{betas:,.3f}")
 #----------------------------------------------------#
+import altair as alt
+g = alt.Chart(df).mark_point().encode(
+    x='exposiciones:Q',
+    y='Pi:Q'
+)
+st.write(g)
+
