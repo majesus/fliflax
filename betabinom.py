4e1c9f3896b6382980ce1a79309621eb37989b20
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-04 12:12:21 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -266,6 +266,10 @@ else:
 
 st.write('###### Anexo 1. Distribución de contactos Pi (y acumulada Ri)')
 #df = df.set_index('exposiciones')
-st.table(df.style.format("{:,.0f}").set_properties(**{'text-align': 'center'}).set_properties(**{'background-color': '#ffffff'})) 
-st.write('Parámetros de forma: alfa: ',f"{alpha:,.3f}",'y beta: ',f"{alpha:,.3f}")
+if st.checkbox("Si deseas ver la tabla completa de valores de Pi y Ri alcanzados, marca la casilla.", False):
+    st.write('###### Anexo. Distribución de contactos Pi (y acumulada Ri)')
+    st.table(df1.style.format("{:,.0f}").set_properties(**{'text-align': 'center'}).set_properties(**{'background-color': '#ffffff'})) 
+    st.info('Parámetros de forma: alfa: ',f"{alpha:,.3f}",'y beta: ',f"{alpha:,.3f}")
+# st.table(df.style.format("{:,.0f}").set_properties(**{'text-align': 'center'}).set_properties(**{'background-color': '#ffffff'})) 
+#st.write('Parámetros de forma: alfa: ',f"{alpha:,.3f}",'y beta: ',f"{alpha:,.3f}")
 #----------------------------------------------------#
