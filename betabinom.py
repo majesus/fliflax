011cd2bce7e370e18ca9244de7df790a8f9c1542
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-06 20:26:12 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -125,9 +125,14 @@ def BetaBinom(a, b, n, x):
 if alphas >= 0 or betas >= 0:
   pmf = BetaBinom(alphas, betas, n, x)
 else:
+  A1 = 500000
+  A2 = 550000
   alphas = 0.125
   betas = 0.125
   pmf = BetaBinom(alphas, betas, n, x)
+  st.error("Ha habido un error. Los valores alfa y beta generan un error en la distribución. Debes revisar la elección de A1 y A2. "
+          "Mientras tanto, los resultados que te mostramos corresponden a un valor alfa igual a 0.125, y un valor beta igual a 0.125.").
+  
 # Pi:
 y = pmf*1000000
 # Ri:
