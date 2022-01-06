6b8aa77600c2f1a8b3e6aa032a9431afc4bc16c3
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-06 11:41:43 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -255,6 +255,7 @@ df1 = df.set_index('exposiciones')
 selected_indices = st.multiselect('Selecciona el/los valor/es i:', df1.index)
 selected_indices = map(lambda selected_indices:selected_indices, selected_indices)
 selected_rows = df1.loc[selected_indices]
+st.markdown("""---""")
 st.write('###### Tabla 2. Valores de Pi y Ri seleccionados')
 # st.table(selected_rows)
 st.table(selected_rows.style.format("{:,.0f}").set_properties(**{'text-align': 'center'}).set_properties(**{'background-color': '#ffffff'})) 
