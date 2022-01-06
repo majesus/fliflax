1bf45d0440f1feab43e3aef10f5480234600f62b
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-06 19:16:53 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -307,5 +307,11 @@ with st.form(key ='Form1'):
     A1 = st.number_input("Audiencia acumulada tras 1 inserción:", min_value = 1, max_value = pow(10, 6), value = 500000, step=100, key = "A1")
   with col2:
     A2 = st.number_input("Audiencia acumulada tras 2 inserciones:", min_value = 1, max_value = pow(10, 6), value = 550000, step=100, key = "A2")
+  col1, col2 = st.columns([5,5])
+  with col1:
+    P = st.number_input("Población:", min_value = pow(10, 6), max_value = pow(10, 10), value = 1000000, step=100, key = "poblacion")
+  with col2:
+    Precio = st.number_input("Precio de una inserción €:", min_value = 1, max_value = pow(10, 10), value = 1000000, step=100, key = "precio")
+    inserciones = st.slider("Inserciones:", 2, 50, value = 5, step=1, key = "inserciones")
   st.form_submit_button("Calcular")
 
