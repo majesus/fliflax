9ad41c15b777f6aadb8ff5d0c94d9d318efadb1f
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-07 07:41:51 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -15,14 +15,14 @@ st.markdown(""" <style> .font {
     color: #000000;} 
     </style> """, unsafe_allow_html=True)
 #----------------------------------------------------#
-with st.form(key ='FormFEM'):
+with st.sidebar.form(key ='FormFEM'):
   LC = 2
   values=['Leal a mi marca', 'Favorable a mi marca', 'Leal/Favorable otras marcas', 'No usuario']
   options=[0, 1, 2, LC+1]
   dic = dict(zip(options, values))
   a = st.sidebar.selectbox('Choose a restaurant', options, format_func=lambda x: dic[x])
   st.sidebar.write("El valor de corrección elegido es", f"**{a:,.0f}**", ".")
-  submitted = st.form_submit_button("Calcular")
+  submitted = st.sidebar.form_submit_button("Calcular")
 #----------------------------------------------------#
 
 st.image('Avatar-con-naming-Fliflax.jpg',width=200)
