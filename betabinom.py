c261436303aa21be62fa0278c79c96a8e58f1715
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-08 19:18:36 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -83,7 +83,7 @@ else:
     FEM = 1 + VA * (PO + NM + ACT + IP)
     st.sidebar.write("2.1) La frecuencia efectiva mínima es", f"**{FEM:,.1f}**", "impactos por persona de la cobertura efectiva.")
   elif PO == 3 and NM == 2 and ACT == 2:
-    st.sidebar.write("")
+    st.sidebar.write("segundo IF ELSE")
     FEM = 1 + VA * (PO + NM + ACT + IP)
     st.sidebar.write("2.3) La frecuencia efectiva mínima es", f"**{FEM:,.1f}**", "impactos por persona de la cobertura efectiva.")
     
