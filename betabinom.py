fdd3b3f165d5d63638c4dbb4370da55e68478091
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-08 19:26:26 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -86,6 +86,8 @@ else:
     st.sidebar.write("segundo IF ELSE")
     PO_cor = 1; NM_cor = 1; ACT_cor = 1
     st.sidebar.write("PO_cor", f"**{PO_cor:,.1f}**", "")
+    st.sidebar.write("NM_cor", f"**{NM_cor:,.1f}**", "")
+    st.sidebar.write("ACT_cor", f"**{ACT_cor:,.1f}**", "")
     FEM = 1 + VA * (PO_cor + NM_cor + ACT_cor + IP + Lider_LC)
     st.sidebar.write("2.3) La frecuencia efectiva mínima es", f"**{FEM:,.1f}**", "impactos por persona de la cobertura efectiva.")
   else:
