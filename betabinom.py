36ba5a064f0cd943a0da0d8ed607ee424740a8c9
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-08 17:37:09 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -44,12 +44,8 @@ with st.sidebar.form(key ='FormFEM'):
     dic0 = dict(zip(options0, values0))
     VA = st.radio('¿Cuál es el valor de atención del medio que elijo?', options0, format_func=lambda x: dic0[x], key = "VA")
     
-    if Lider_LC + 1 == 2: # LC + 100
-      values1=['Leal a mi marca', 'Favorable a mi marca', 'Leal a otra marca / No usuario']
-      options1=[0, 1, 2]
-    else:
-      values1=['Leal a mi marca', 'Favorable a mi marca', 'Leal a otra marca", "No usuario']
-      options1=[0, 1, 2, LC + 100]
+    values1=['Leal a mi marca', 'Favorable a mi marca', 'Leal a otra marca", "No usuario']
+    options1=[0, 1, 2, LC + 100]
     dic1 = dict(zip(options1, values1))
     PO = st.radio('¿Cuál es la población a que me dirijo?', options1, format_func=lambda x: dic1[x], key = "PO")
     
