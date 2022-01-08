b6aee314e014b82d7f377c3915ad01c59cf0146d
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-08 12:32:30 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -89,12 +89,12 @@ else:
   st.sidebar.write("NM valor de corrección", f"**{NM:,.1f}**", "")
   st.sidebar.write("ACT valor de corrección", f"**{ACT:,.1f}**", "")
   st.sidebar.write("IP valor de corrección", f"**{IP:,.1f}**", "")
-  FEM = 1 + VA * (PO + NM + ACT + IP + LC)
-  #if PO == LC + 100 or NM == LC + 100 or ACT == LC + 100:
+  # FEM = 1 + VA * (PO + NM + ACT + IP + LC)
+  if PO == LC + 100 or NM == LC + 100 or ACT == LC + 100:
     #LC = 1
     #FEM = 1 + VA * (PO + NM + ACT + IP + LC)
-  #else:
-    #FEM = 1 + VA * (PO + NM + ACT + IP)
+  else:
+    FEM = 1 + VA * (PO + NM + ACT + IP)
   st.sidebar.write("2) La frecuencia efectiva mínima es", f"**{FEM:,.1f}**", "impactos por persona de la cobertura efectiva.")
 
   
