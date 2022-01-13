d7e56411098cd31be94760c565b28c10caed91fa
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-13 16:34:07 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -12,6 +12,13 @@ st.set_page_config(#layout="centered",
                    page_icon=img,
                    initial_sidebar_state='expanded'
                    )
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 #----------------------------------------------------#
 st.markdown(
   """ 
