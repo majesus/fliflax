577d24d5ea7f25dd5de7507851673f8195262f75
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-14 12:28:37 +0100

    Update betabinom.py

--- betabinom.py
+++ betabinom.py
@@ -443,7 +443,7 @@ st_echarts(options=options, width="100%", key=0)
 
 # f"**{df['Ri'].sum() / df['Ri'].iloc[0]:,.0f}**"
 
-value2 = df['Ri'].sum() * 100 / df['Ri'].iloc[0] / n
+value2 = ((df['Ri'].sum() * 100) / df['Ri'].iloc[0]) / n
 options2 = {
         "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
         "series": [
@@ -456,8 +456,8 @@ options2 = {
                     },
                 },
                 "progress": {"show": "true", "width": 10},
-                "detail": {"valueAnimation": "true", "formatter": "{value}"},
-                "data": [{"value": value2, "name": "Frecuencia %"}],
+                "detail": {"valueAnimation": "true", "formatter": "{value2}"},
+                "data": [{"value2": value2, "name": "Frecuencia %"}],
             }
         ],
     }
