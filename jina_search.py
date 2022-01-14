306d57e52680ef48c4adbabe90c12f09cf9e7920
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-14 22:29:48 +0100

    Delete jina_search.py

--- jina_search.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from streamlit_jina import jina
-st.set_page_config(page_title="Jina Text Search",)
-
-endpoint = "http://0.0.0.0:45678/api/search" # This is Jina's default endpoint. If your Flow uses something different, switch it out
-
-st.title("Jina Text Search")
-
-jina.text_search(endpoint=endpoint)
