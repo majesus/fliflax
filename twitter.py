91c702878cc229b97008091daf2ed665adc68fad
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-16 15:55:18 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -18,9 +18,7 @@ api = tw.API(auth)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    # pipe = pipeline(task="sentiment-analysis", model="daveni/twitter-xlm-roberta-emotion-es")
-    model_path="daveni/twitter-xlm-roberta-emotion-es"
-    pipe = pipeline(task="sentiment-analysis", framework="pt", model=model_path, tokenizer=model_path)
+    pipe = pipeline(task="sentiment-analysis", model="daveni/twitter-xlm-roberta-emotion-es")
     return pipe
 
 
