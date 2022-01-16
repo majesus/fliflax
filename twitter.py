93e632846bc5bcc1795714cfdaf01920385ad580
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-16 10:49:10 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -3,6 +3,11 @@ import streamlit as st
 import pandas as pd
 from transformers import pipeline
 
+st.title('Live Twitter Sentiment Analysis with Tweepy and HuggingFace Transformers')
+st.markdown("This app uses tweepy to get tweets from twitter based on the input name/phrase. "
+            "It then processes the tweets through HuggingFace transformers pipeline function for sentiment analysis. "
+            "The resulting sentiments and corresponding tweets are then put in a dataframe for display which is what you see as result.")
+
 st.write("consumerKey:", st.secrets["consumerKey"])
 st.write("consumerSecret:", st.secrets["consumerSecret"])
 st.write("access_token:", st.secrets["access_token"])
@@ -17,7 +22,3 @@ api = tw.API(auth, wait_on_rate_limit=True)
 
 classifier = pipeline('sentiment-analysis')
 
-st.title('Live Twitter Sentiment Analysis with Tweepy and HuggingFace Transformers')
-st.markdown("This app uses tweepy to get tweets from twitter based on the input name/phrase. "
-            "It then processes the tweets through HuggingFace transformers pipeline function for sentiment analysis. "
-            "The resulting sentiments and corresponding tweets are then put in a dataframe for display which is what you see as result.")
