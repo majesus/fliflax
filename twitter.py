b5195b90d2d9f958ff728cfab0eeeb1ceae42da9
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-16 18:33:48 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -19,5 +19,7 @@ api = tw.API(auth)
 from pysentimiento import create_analyzer
 analyzer = create_analyzer(task="sentiment", lang="es")
 results = analyzer.predict("Qué gran jugador es Messi")
+st.write(results)
+
 df = pd.DataFrame(list(results))
 st.table(df)
