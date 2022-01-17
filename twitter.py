2a41dc22e03370cbbf4f03372f3d91492167150e
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-17 15:30:50 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -7,10 +7,10 @@ import os
 
 # https://huggingface.co/spaces/lewtun/twitter-sentiments/blob/aa8bd7daee9993846d1a2330b163aa76b6690023/app.py
 
-# st.write("Has environment variables been set:", os.environ["consumerKey"] == st.secrets["consumerKey"])
-# st.write("Has environment variables been set:", os.environ["consumerSecret"] == st.secrets["consumerSecret"])
-# st.write("Has environment variables been set:", os.environ["access_token"] == st.secrets["access_token"])
-# st.write("Has environment variables been set:", os.environ["access_token_secret"] == st.secrets["access_token_secret"])
+st.write("Has environment variables been set:", os.environ["consumerKey"] == st.secrets["consumerKey"])
+st.write("Has environment variables been set:", os.environ["consumerSecret"] == st.secrets["consumerSecret"])
+st.write("Has environment variables been set:", os.environ["access_token"] == st.secrets["access_token"])
+st.write("Has environment variables been set:", os.environ["access_token_secret"] == st.secrets["access_token_secret"])
 
 auth = tw.OAuthHandler(st.secrets["consumerKey"], st.secrets["consumerSecret"])
 auth.set_access_token(st.secrets["access_token"], st.secrets["access_token_secret"])
