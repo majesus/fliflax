afb0bcf8fcc42b97c541379a5f8c81afc488bdc4
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-17 15:20:51 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -16,3 +16,29 @@ auth = tw.OAuthHandler(st.secrets["consumerKey"], st.secrets["consumerSecret"])
 auth.set_access_token(st.secrets["access_token"], st.secrets["access_token_secret"])
 api = tw.API(auth)
 
+with st.sidebar.form("my_form"):
+    st.write("Inside the form")
+    username = st.text_input(label="username")
+    count = st.slider("count", min_value=10, max_value=100, value=10, step=10)
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Descargar")
+    
+tweets = tw.Cursor(
+        api.user_timeline,
+        screen_name=username,
+        tweet_mode="extended",
+        exclude_replies=True,
+        include_rts=False,).items(count)
+
+tweets = list(tweets)
+response = {
+  "tweets": [tweet.full_text.replace("\n", "").lower() for tweet in tweets],
+  "timestamps": [str(tweet.created_at) for tweet in tweets],
+  "retweets": [tweet.retweet_count for tweet in tweets],
+  "likes": [tweet.favorite_count for tweet in tweets],
+}
+return response
+  
+results = pd.DataFrame(response)
+st.table(results)
