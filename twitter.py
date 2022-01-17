fb8230a8309d066019c58268881624fb34f2876d
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-17 16:16:17 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -2,23 +2,18 @@ import pandas as pd
 import streamlit as st
 import tweepy as tw
 from transformers import pipeline
-
 import os
-
-st.write("Hola")
 
 # https://huggingface.co/spaces/lewtun/twitter-sentiments/blob/aa8bd7daee9993846d1a2330b163aa76b6690023/app.py
 
-st.write("Has environment variables been set:", os.environ["consumerKey"] == st.secrets["consumerKey"])
-st.write("Has environment variables been set:", os.environ["consumerSecret"] == st.secrets["consumerSecret"])
-st.write("Has environment variables been set:", os.environ["access_token"] == st.secrets["access_token"])
-st.write("Has environment variables been set:", os.environ["access_token_secret"] == st.secrets["access_token_secret"])
+# st.write("Has environment variables been set:", os.environ["consumerKey"] == st.secrets["consumerKey"])
+# st.write("Has environment variables been set:", os.environ["consumerSecret"] == st.secrets["consumerSecret"])
+# st.write("Has environment variables been set:", os.environ["access_token"] == st.secrets["access_token"])
+# st.write("Has environment variables been set:", os.environ["access_token_secret"] == st.secrets["access_token_secret"])
 
 auth = tw.OAuthHandler(st.secrets["consumerKey"], st.secrets["consumerSecret"])
 auth.set_access_token(st.secrets["access_token"], st.secrets["access_token_secret"])
 api = tw.API(auth)
-
-
 
 with st.sidebar.form("my_form"):
     st.write("Inside the form")
