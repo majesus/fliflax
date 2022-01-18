00d5653620525422d8109da564787daf30279656
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-18 16:24:52 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -67,3 +67,14 @@ if st.checkbox("Si deseas ver la representación gráfica de 'likes' por fecha."
   st.altair_chart(chart, use_container_width=True)
 
 st.markdown("----")
+
+# https://altair-viz.github.io/user_guide/times_and_dates.html
+if st.checkbox("Si deseas ver la representación gráfica de 'retweets' por fecha.", False):
+  import altair as alt
+  st.markdown("----")
+  chart = alt.Chart(results).mark_area(opacity=0.3,).encode(
+    x=alt.X('timestamps:T', title='', axis=alt.Axis(labelOverlap="greedy",grid=False)),
+    y=alt.Y('retweets', scale=alt.Scale(type='log')))
+  st.altair_chart(chart, use_container_width=True)
+
+st.markdown("----")
