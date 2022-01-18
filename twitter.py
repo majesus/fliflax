746fed1b8700e9924e2b0bb821c6527c06785af9
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-18 21:37:58 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -111,9 +111,5 @@ if st.checkbox("Si deseas ver la representación gráfica de 'retweets' por fech
 st.markdown("----")
 
 results['date'] = pd.to_datetime(results['timestamps']) - pd.to_timedelta(7, unit='d')
-df = df['likes']
-       .sum()
-       .reset_index()
-       .sort_values('date')
-#df = results.resample('W-Mon', on='date').sum().reset_index().sort_values(by='date')
+df = results.resample('W-Mon', on='date').sum().reset_index().sort_values(by='date')
 st.table(df)
