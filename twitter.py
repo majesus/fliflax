0d166068f98f285a1496d18cf120bce53aa261bb
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-19 23:31:12 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -40,9 +40,11 @@ api = tw.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 #-----------------------------------------------------------------#
 with st.sidebar.form("my_form"):
-    st.write("**Tweet Finder**:")
-    username = st.text_input(label="Cuenta a buscar ...", value = "JoeBiden")
+    st.write("**Buscador de Tweets**:")
+    username = st.text_input(label="¿QUé deseas buscar?", value = "JoeBiden")
     count = st.slider("Hasta un máximo de ...", min_value=1, max_value=600, value=20, step=10)
+    
+    tipo = st.radio("¿Qué deseas descargar?", ('Usuario', 'Tema'), key = "type")
     
     values1=['Yes','No']
     options1=[True,False]
@@ -115,7 +117,7 @@ def searchTweets(username, count):
 
     return results 
       
-if type == 1:
+if tipo == "Usuario":
   results = get_tweets(username, count)
 else:
    results = searchTweets(username, count)
