91794c34aa8c5fe5c5517aa5fd2e1dacd050bcd7
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-21 20:01:46 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -11,11 +11,10 @@ __email__     = "majesus@us.es"
 #-----------------------------------------------------------------#
 import pandas as pd
 import streamlit as st
-import tweepy as tw
 import altair as alt
-from transformers import pipeline
-import os
-
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+#-----------------------------------------------------------------#
 from PIL import Image
 img=Image.open('img/fliflax-logo.jpg')
 st.set_page_config(#layout="centered",
@@ -37,18 +36,6 @@ st.write("Abajo te mostramos, por ejemplo, unas métricas sencillas de Twitter, 
 
 st.markdown("----")
 #-----------------------------------------------------------------#
-
-# https://huggingface.co/spaces/lewtun/twitter-sentiments/blob/aa8bd7daee9993846d1a2330b163aa76b6690023/app.py
-
-# st.write("Has environment variables been set:", os.environ["consumerKey"] == st.secrets["consumerKey"])
-# st.write("Has environment variables been set:", os.environ["consumerSecret"] == st.secrets["consumerSecret"])
-# st.write("Has environment variables been set:", os.environ["access_token"] == st.secrets["access_token"])
-# st.write("Has environment variables been set:", os.environ["access_token_secret"] == st.secrets["access_token_secret"])
-
-auth = tw.OAuthHandler(st.secrets["consumerKey"], st.secrets["consumerSecret"])
-auth.set_access_token(st.secrets["access_token"], st.secrets["access_token_secret"])
-api = tw.API(auth, wait_on_rate_limit=True)
-
 #-----------------------------------------------------------------#
 import datetime as dt
 
@@ -75,61 +62,105 @@ with st.sidebar.form("my_form"):
 
     submitted = st.form_submit_button("Descargar")
 #-----------------------------------------------------------------#
-
-#@st.cache(suppress_st_warning=False)
-
-def searchTweets(username, count):
-  tweets = api.search_tweets(q=username, count=count, fromDate=fecha)
-  
-  tweets = list(tweets)
-  response = {
-    "tweets": [tweet.text.replace("\n", "").lower() for tweet in tweets],
-    "timestamps": [str(tweet.created_at) for tweet in tweets],
-    #"retweets": [tweet.retweet_count for tweet in tweets],
-    #"likes": [tweet.favorite_count for tweet in tweets],
-    #"retweet_text": [tweet.retweeted_status.full_text.replace("\n", "").lower() for tweet in tweets],
-    "screen_name": [tweet.user.name for tweet in tweets],
-    #"query": [query for tweet in tweets],
-    #"hashtags": [tweet.hashtags for tweet in tweets],
-    #"status_count": [tweet.status_count for tweet in tweets],
-    "location": [tweet.user.location for tweet in tweets],
-    "verified": [tweet.user.verified for tweet in tweets],
-    #"source_device": [tweet.source_device for tweet in tweets],
-  }
-  results = pd.DataFrame(response) 
-  return results 
-
-def get_tweets(username, count):
-  tweets = tw.Cursor(
-    api.user_timeline,
-    screen_name=username,
-    tweet_mode="extended",
-    exclude_replies=replies,
-    include_rts=retweets,
-  ).items(count)
-  
-  tweets = list(tweets)
-  response = {
-    "tweets": [tweet.full_text.replace("\n", "").lower() for tweet in tweets],
-    "timestamps": [str(tweet.created_at) for tweet in tweets],
-    "retweets": [tweet.retweet_count for tweet in tweets],
-    "likes": [tweet.favorite_count for tweet in tweets],
-    #"retweet_text": [tweet.retweeted_status.full_text.replace("\n", "").lower() for tweet in tweets],
-    "screen_name": [tweet.user.screen_name for tweet in tweets],
-    #"query": [query for tweet in tweets],
-    #"hashtags": [tweet.hashtags for tweet in tweets],
-    #"status_count": [tweet.status_count for tweet in tweets],
-    #"location": [tweet.location for tweet in tweets],
-    #"source_device": [tweet.source_device for tweet in tweets],
-    }
-  results = pd.DataFrame(response) 
-  return results 
-
-
+# Creating list to append tweet data 
+tweets_list1 = []
+# https://github.com/JustAnotherArchivist/snscrape/blob/master/snscrape/modules/twitter.py
+# Using TwitterSearchScraper to scrape data and append tweets to list
 if tipo == "Usuario":
-  results = get_tweets(username, count)
+  for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:'+username).get_items()): 
+    if i>count: #number of tweets you want to scrape
+      break
+    tweets_list1.append([tweet.date, 
+                         tweet.id, 
+                         tweet.content, 
+                         tweet.user.username,
+                         tweet.retweetCount,
+                         tweet.retweetedTweet,
+                         tweet.renderedContent,
+                         tweet.outlinks,
+                         tweet.tcooutlinks,
+                         tweet.replyCount,
+                         tweet.likeCount,
+                         tweet.quoteCount,
+                         tweet.conversationId,
+                         tweet.lang,
+                         tweet.source,
+                         tweet.sourceUrl,
+                         tweet.sourceLabel,
+                         tweet.media,
+                         tweet.quotedTweet,
+                         tweet.mentionedUsers,
+                         tweet.url,
+                         tweet.coordinates,
+                         tweet.place,
+                         tweet.hashtags,
+                         tweet.cashtags,
+                         tweet.inReplyToTweetId,
+                         tweet.inReplyToUser
+                         ]) #declare the attributes to be returned
 elif tipo == "Tema":
-  results = searchTweets(username, count)
+  for i,tweet in enumerate(sntwitter.TwitterSearchScraper(username).get_items()): 
+    if i>count: #number of tweets you want to scrape
+      break
+    tweets_list1.append([tweet.date, 
+                         tweet.id, 
+                         tweet.content, 
+                         tweet.user.username,
+                         tweet.retweetCount,
+                         tweet.retweetedTweet,
+                         tweet.renderedContent,
+                         tweet.outlinks,
+                         tweet.tcooutlinks,
+                         tweet.replyCount,
+                         tweet.likeCount,
+                         tweet.quoteCount,
+                         tweet.conversationId,
+                         tweet.lang,
+                         tweet.source,
+                         tweet.sourceUrl,
+                         tweet.sourceLabel,
+                         tweet.media,
+                         tweet.quotedTweet,
+                         tweet.mentionedUsers,
+                         tweet.url,
+                         tweet.coordinates,
+                         tweet.place,
+                         tweet.hashtags,
+                         tweet.cashtags,
+                         tweet.inReplyToTweetId,
+                         tweet.inReplyToUser
+                         ]) #declare the attributes to be returned
+
+# Creating a dataframe from the tweets list above 
+results = pd.DataFrame(tweets_list1, 
+                          columns=['Datetime',
+                                   'Tweet Id',
+                                   'Text',
+                                   'Username',
+                                   'retweetCount',
+                                   'retweetedTweet',
+                                   'renderedContent',
+                                   'outlinks',
+                                   'tcooutlinks',
+                                   'replyCount',
+                                   'likeCount',
+                                   'quoteCount',
+                                   'conversationId',
+                                   'lang',
+                                   'source',
+                                   'sourceUrl',
+                                   'sourceLabel',
+                                   'media',
+                                   'quotedTweet',
+                                   'mentionedUsers',
+                                   'url',
+                                   'coordinates',
+                                   'place',
+                                   'hashtags',
+                                   'cashtags',
+                                   'inReplyToTweetId',
+                                   'inReplyToUser'
+                                   ])
 #-----------------------------------------------------------------#
 
 if st.checkbox("If you want to see the tweets downloaded by date, click here.", False):
@@ -143,10 +174,10 @@ st.markdown("----")
 if st.checkbox("Si deseas ver la representación gráfica de los retweets, haz clic aquí.", False):
   #import altair as alt
   st.markdown("----")
-  results.loc[results.likes == 0, 'likes'] = 0.001 # log
+  results.loc[results.retweetCount == 0, 'retweetCount'] = 0.001 # log
   chart = alt.Chart(results).mark_area(color = "lightblue", interpolate = "step-after", line = True, opacity=0.3,).encode(
     x=alt.X('timestamps:T', title='', axis=alt.Axis(labelOverlap="greedy",grid=False)),
-    y=alt.Y('likes', scale=alt.Scale(type='log')))
+    y=alt.Y('retweetCount', scale=alt.Scale(type='log')))
   st.altair_chart(chart, use_container_width=True)
 
 st.markdown("----")
@@ -154,10 +185,10 @@ st.markdown("----")
 if st.checkbox("Si deseas ver la representación gráfica de los likes, haz clic aquí.", False):
   #import altair as alt
   st.markdown("----")
-  results.loc[results.retweets == 0, 'retweets'] = 0.001 # log
+  results.loc[results.likeCount == 0, 'likeCount'] = 0.001 # log
   chart = alt.Chart(results).mark_area(color = "lightblue", interpolate = "step-after", line = True, opacity=0.3,).encode(
     x=alt.X('timestamps:T', title='', axis=alt.Axis(labelOverlap="greedy",grid=False)),
-    y=alt.Y('retweets', scale=alt.Scale(type='log')))
+    y=alt.Y('likeCount', scale=alt.Scale(type='log')))
   st.altair_chart(chart, use_container_width=True)
   
 st.markdown("----")
@@ -167,7 +198,7 @@ st.write("#### A continuación, te mostramos la evolución semanal de retweets y
 
 st.markdown("----")
 
-results['start_date'] = pd.to_datetime(results['timestamps']) - pd.to_timedelta(7, unit='d')
+results['start_date'] = pd.to_datetime(results['Datetime']) - pd.to_timedelta(7, unit='d')
 df = results.resample('W-Mon', on='start_date').sum().reset_index().sort_values(by='start_date')
 st.table(df)
 
@@ -176,30 +207,30 @@ st.markdown("----")
 
 st.write("#### Aquí te presentamos las principales métricas.")
 
-value1 = df['retweets'].iloc[len(df) - 2]
-value2 = df['retweets'].iloc[len(df) - 3]
+value1 = df['retweetCount'].iloc[len(df) - 2]
+value2 = df['retweetCount'].iloc[len(df) - 3]
 delta = round((value2 - value1) / (value2 + 1)) # división por 0
 delta = "{:.0%}".format(delta)
 
 col1, col2 = st.columns([5,5])
 with col1:
-  value1 = df['retweets'].iloc[len(df) - 2]
-  if df['retweets'].iloc[len(df) - 3] == 0:
-    value2 = df['retweets'].iloc[len(df) - 3] + 1
+  value1 = df['retweetCount'].iloc[len(df) - 2]
+  if df['retweetCount'].iloc[len(df) - 3] == 0:
+    value2 = df['retweetCount'].iloc[len(df) - 3] + 1
   else:
-    value2 = df['retweets'].iloc[len(df) - 3]
+    value2 = df['retweetCount'].iloc[len(df) - 3]
   delta = round((value2 - value1) / (value2), 1) 
   delta = "{:.0%}".format(delta)
   st.write("", f"**{value2:,.3f}**", "")
-  st.metric(label="retweets", value='{:,}'.format(value1), delta=delta, delta_color="inverse")
+  st.metric(label="retweetCount", value='{:,}'.format(value1), delta=delta, delta_color="inverse")
 with col2:
-  value3 = df['likes'].iloc[len(df) - 2]
-  if df['likes'].iloc[len(df) - 3] == 0:
-    value4 = df['likes'].iloc[len(df) - 3] + 1
+  value3 = df['likeCount'].iloc[len(df) - 2]
+  if df['likeCount'].iloc[len(df) - 3] == 0:
+    value4 = df['likeCount'].iloc[len(df) - 3] + 1
   else:
-    value4 = df['likes'].iloc[len(df) - 3]
+    value4 = df['likeCount'].iloc[len(df) - 3]
   delta = round((value4 - value3) / (value4 ), 1) 
   delta = "{:.0%}".format(delta)
   st.write("", f"**{value2:,.3f}**", "")
-  st.metric(label="likes", value='{:,}'.format(value3), delta=delta, delta_color="inverse")
+  st.metric(label="likeCount", value='{:,}'.format(value3), delta=delta, delta_color="inverse")
 #-----------------------------------------------------------------#
