e336ac731db26f38a61fd98f09045aa7112d9c20
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-21 20:48:24 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -12,9 +12,8 @@ __email__     = "majesus@us.es"
 import pandas as pd
 import streamlit as st
 import altair as alt
-import snscrape
 import snscrape.modules.twitter as sntwitter
-import pandas as pd
+import itertools
 #-----------------------------------------------------------------#
 from PIL import Image
 img=Image.open('img/fliflax-logo.jpg')
@@ -63,142 +62,17 @@ with st.sidebar.form("my_form"):
 
     submitted = st.form_submit_button("Descargar")
 #-----------------------------------------------------------------#
-# Creating list to append tweet data 
-tweets_list1 = []
-# https://github.com/JustAnotherArchivist/snscrape/blob/master/snscrape/modules/twitter.py
-# Using TwitterSearchScraper to scrape data and append tweets to list
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:jack').get_items()): 
-  if i>count: #number of tweets you want to scrape
-    break
-  tweets_list1.append([tweet.date, 
-                       tweet.id, 
-                       tweet.content, 
-                       tweet.user.username,
-                       tweet.retweetCount,
-                       tweet.retweetedTweet,
-                       tweet.renderedContent,
-                       tweet.outlinks,
-                       tweet.tcooutlinks,
-                       tweet.replyCount,
-                       tweet.likeCount,
-                       tweet.quoteCount,
-                       tweet.conversationId,
-                       tweet.lang,
-                       tweet.source,
-                       tweet.sourceUrl,
-                       tweet.sourceLabel,
-                       tweet.media,
-                       tweet.quotedTweet,
-                       tweet.mentionedUsers,
-                       tweet.url,
-                       tweet.coordinates,
-                       tweet.place,
-                       tweet.hashtags,
-                       tweet.cashtags,
-                       tweet.inReplyToTweetId,
-                       tweet.inReplyToUser
-                      ]) 
+# our search term, using syntax for Twitter's Advanced Search
+search = username
 
-# Creating a dataframe from the tweets list above 
-results = pd.DataFrame(tweets_list1, 
-                          columns=['Datetime',
-                                   'Tweet Id',
-                                   'Text',
-                                   'Username',
-                                   'retweetCount',
-                                   'retweetedTweet',
-                                   'renderedContent',
-                                   'outlinks',
-                                   'tcooutlinks',
-                                   'replyCount',
-                                   'likeCount',
-                                   'quoteCount',
-                                   'conversationId',
-                                   'lang',
-                                   'source',
-                                   'sourceUrl',
-                                   'sourceLabel',
-                                   'media',
-                                   'quotedTweet',
-                                   'mentionedUsers',
-                                   'url',
-                                   'coordinates',
-                                   'place',
-                                   'hashtags',
-                                   'cashtags',
-                                   'inReplyToTweetId',
-                                   'inReplyToUser'
-                                   ])
+# the scraped tweets, this is a generator
+scraped_tweets = sntwitter.TwitterSearchScraper(search).get_items()
+
+# slicing the generator to keep only the first 100 tweets
+sliced_scraped_tweets = itertools.islice(scraped_tweets, count)
+
+# convert to a DataFrame and keep only relevant columns
+results = pd.DataFrame(sliced_scraped_tweets)[['date', 'content','retweetCount','likeCount']]
 #-----------------------------------------------------------------#
 
-if st.checkbox("If you want to see the tweets downloaded by date, click here.", False):
-  st.markdown("----")
-  st.table(results)
-  
-st.markdown("----")
-
-# https://altair-viz.github.io/user_guide/times_and_dates.html
-
-if st.checkbox("Si deseas ver la representación gráfica de los retweets, haz clic aquí.", False):
-  #import altair as alt
-  st.markdown("----")
-  results.loc[results.retweetCount == 0, 'retweetCount'] = 0.001 # log
-  chart = alt.Chart(results).mark_area(color = "lightblue", interpolate = "step-after", line = True, opacity=0.3,).encode(
-    x=alt.X('timestamps:T', title='', axis=alt.Axis(labelOverlap="greedy",grid=False)),
-    y=alt.Y('retweetCount', scale=alt.Scale(type='log')))
-  st.altair_chart(chart, use_container_width=True)
-
-st.markdown("----")
-
-if st.checkbox("Si deseas ver la representación gráfica de los likes, haz clic aquí.", False):
-  #import altair as alt
-  st.markdown("----")
-  results.loc[results.likeCount == 0, 'likeCount'] = 0.001 # log
-  chart = alt.Chart(results).mark_area(color = "lightblue", interpolate = "step-after", line = True, opacity=0.3,).encode(
-    x=alt.X('timestamps:T', title='', axis=alt.Axis(labelOverlap="greedy",grid=False)),
-    y=alt.Y('likeCount', scale=alt.Scale(type='log')))
-  st.altair_chart(chart, use_container_width=True)
-  
-st.markdown("----")
-#-----------------------------------------------------------------#
-
-st.write("#### A continuación, te mostramos la evolución semanal de retweets y likes.")
-
-st.markdown("----")
-
-results['start_date'] = pd.to_datetime(results['Datetime']) - pd.to_timedelta(7, unit='d')
-df = results.resample('W-Mon', on='start_date').sum().reset_index().sort_values(by='start_date')
-st.table(df)
-
-st.markdown("----")
-#-----------------------------------------------------------------#
-
-st.write("#### Aquí te presentamos las principales métricas.")
-
-value1 = df['retweetCount'].iloc[len(df) - 2]
-value2 = df['retweetCount'].iloc[len(df) - 3]
-delta = round((value2 - value1) / (value2 + 1)) # división por 0
-delta = "{:.0%}".format(delta)
-
-col1, col2 = st.columns([5,5])
-with col1:
-  value1 = df['retweetCount'].iloc[len(df) - 2]
-  if df['retweetCount'].iloc[len(df) - 3] == 0:
-    value2 = df['retweetCount'].iloc[len(df) - 3] + 1
-  else:
-    value2 = df['retweetCount'].iloc[len(df) - 3]
-  delta = round((value2 - value1) / (value2), 1) 
-  delta = "{:.0%}".format(delta)
-  st.write("", f"**{value2:,.3f}**", "")
-  st.metric(label="retweetCount", value='{:,}'.format(value1), delta=delta, delta_color="inverse")
-with col2:
-  value3 = df['likeCount'].iloc[len(df) - 2]
-  if df['likeCount'].iloc[len(df) - 3] == 0:
-    value4 = df['likeCount'].iloc[len(df) - 3] + 1
-  else:
-    value4 = df['likeCount'].iloc[len(df) - 3]
-  delta = round((value4 - value3) / (value4 ), 1) 
-  delta = "{:.0%}".format(delta)
-  st.write("", f"**{value2:,.3f}**", "")
-  st.metric(label="likeCount", value='{:,}'.format(value3), delta=delta, delta_color="inverse")
 #-----------------------------------------------------------------#
