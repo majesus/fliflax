f20481e6be1a9ca3c822e7a35c293203a11b3269
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-22 20:15:27 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -61,7 +61,7 @@ for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:'+username).get_it
     break
   tweets_list1.append([tweet.id,
                        tweet.content,
-                       tweet.date,tweet.retweetCount])
+                       tweet.date])
 
 tweets_df1 = pd.DataFrame(tweets_list1)
 st.table(tweets_df1)
