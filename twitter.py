a97a9044acf162f4cf2cc23c93b3bc7998b5b679
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-22 09:31:53 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -60,7 +60,7 @@ users_name = 'currovillarejo'
 for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:'+users_name).get_items()):
   if i>20:
     break
-  tweets_list1.append([tweet.id, tweet.content, tweet.lang])
+  tweets_list1.append([tweet.id, tweet.content, tweet.user.username])
 
 tweets_df1 = pd.DataFrame(tweets_list1)
 st.table(tweets_df1)
