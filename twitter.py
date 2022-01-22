975179e89e9b5fe2848190471101054795099d7d
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-22 09:25:29 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -61,9 +61,7 @@ for n, k in enumerate(users_name):
     for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:{}'.format(users_name[n])).get_items()):
       if i>10:
         break
-      tweets_list1.append([tweet.retweetCount, 
-                           tweet.id, 
-                           tweet.content])
+      tweets_list1.append([tweet.retweetCount, tweet.id, tweet.content])
 
 tweets_df1 = pd.DataFrame(tweets_list1)
 st.table(tweets_df1)
