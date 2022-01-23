f5228f1c14daf441f965d6e3b7d8b6ef477226ad
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-23 12:14:02 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -27,23 +27,7 @@ st.write("Abajo te mostramos, por ejemplo, unas métricas sencillas de Twitter, 
 
 st.markdown("----")
 #-----------------------------------------------------------------#
-with st.form(key=’Twitter_form’):
-      search_term = st.text_input(‘What do you want to search for?’)
-      limit = st.slider(‘How many tweets do you want to get?’, 
-                         min, 
-                         max, 
-                         step=int)
-      output_csv = st.radio(‘Save a CSV file?’, 
-                             [‘Yes’, ‘No’])
-      file_name = st.text_input(‘Name the CSV file:’)
-      submit_button = st.form_submit_button(label=’Search’)
-import twint
-c = twint.Config()
-c.Search = search_term
-c.Limit = limit
-twint.run.Search(c)
-data = pd.read_csv(f'{file_name}.csv')
-st.table(data)
+
 #-----------------------------------------------------------------#
 import datetime as dt
 
@@ -71,29 +55,6 @@ with st.sidebar.form("my_form"):
 
     submitted = st.form_submit_button("Descargar")
 #-----------------------------------------------------------------#
-import twint
-with st.form(key=’Twitter_form’):
-      search_term = st.text_input(‘What do you want to search for?’)
-      limit = st.slider(‘How many tweets do you want to get?’, 
-                         min, 
-                         max, 
-                         step=int)
-      output_csv = st.radio(‘Save a CSV file?’, 
-                             [‘Yes’, ‘No’])
-      file_name = st.text_input(‘Name the CSV file:’)
-      submit_button = st.form_submit_button(label=’Search’)
-      
-if submit_button:
-        c = twint.Config()
-        c.Search = search_term
-        c.Limit = limit
-        c.Store_csv = True
-        if c.Store_csv:
-            c.Output = f'{file_name}.csv'
-        twint.run.Search(c)
-        
-data = pd.read_csv(f'{file_name}.csv')
-st.table(data)
 #-----------------------------------------------------------------#
 # https://github.com/JustAnotherArchivist/snscrape/blob/master/snscrape/modules/twitter.py
 # Using TwitterSearchScraper to scrape data and append tweets to list
