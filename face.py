11a67a8deeaef54a8513ae05dd795bb5ca4b4843
Manuel J. Sánchez-Franco <majesus@us.es>
2022-01-29 11:28:07 +0100

    Update face.py

--- face.py
+++ face.py
@@ -9,11 +9,3 @@ st.write('''
 ''')
 st.write("A Image Classification Web App That Detects the Emotions Based On An Image")
 file = st.file_uploader("Please Upload an image of Person With Face", type=['jpg','png'])
-if file is None:
-  st.text("Please upload an image file")
-else:
-  image = Image.open(file)
-  detector = FER(mtcnn=True)
-  result = detector.detect_emotions(image)
-  st.write(result)
-  st.image(image, use_column_width=True)
