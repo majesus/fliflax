1bcbb7c96a4d6b92babef76df7967ab23d5b48d2
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-18 23:55:19 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -110,7 +110,7 @@ for s, i in sims[:10]:
     dat= [str(s), labs[i], text[i]]
     L.append(dat)
 df = pd.DataFrame(L, columns = ['cosine', 'type', 'string'])
-df.cosine = df.cosine.round(4)
+df.cosine = df.cosine.round()
 st.table(df) 
     
 st.markdown("""---""")
