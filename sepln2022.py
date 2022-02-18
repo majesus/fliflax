8e0ac15424117c4bb4ea140034c2213541c5ee3c
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-18 19:19:25 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -3,33 +3,19 @@ import streamlit as st
 from collections import Counter
 import pandas as pd
 import re
-
+#---------------------------------------------------------#
 from sentence_transformers import SentenceTransformer, util
 #@st.cache
 model = SentenceTransformer('msmarco-MiniLM-L-12-v3')
-
 st.write("cargado")
-
-
-# Cargamos los datos de nuevos
-
-# None para usar todas => ¡Tarda mucho!
-
-n_top = 10
-query = 'enduring relationship'
-
-with st.form(key='my_form'):
-    n_top = st.text_input(label='número de frases')
-    query = st.text_input(label='frase objetivo')
-    form1 = st.form_submit_button(label='Calcular')
-
+#---------------------------------------------------------#
 # Decide si se enmascaran palabras o no
 masking = False
-
+#---------------------------------------------------------#
 datos = pd.read_csv("csv/proyecto.csv")
 airbnb = datos[datos.type=="airbnb"].description1 
 hotel = datos[datos.type=="hotel"].description1 
-
+#---------------------------------------------------------#
 def split_sentences(reviews):
     res = []
     for r in reviews:
@@ -47,9 +33,17 @@ def mask(s):
         return ' '.join(res)
     else:
         return s
-
+#---------------------------------------------------------#
 airbnb_sents = split_sentences(airbnb)
 hotel_sents = split_sentences(hotel)
+#---------------------------------------------------------#
+n_top = 10
+query = 'enduring relationship'
+
+with st.form(key='my_form'):
+    n_top = st.text_input(label='número de frases')
+    query = st.text_input(label='frase objetivo')
+    form1 = st.form_submit_button(label='Calcular')
 
 if n_top:
     st.write("Codificando",n_top,"frases...")
@@ -64,17 +58,22 @@ else:
     emb = model.encode(text)
     st.write("Done")
     
- 
+#---------------------------------------------------------#
 #st.table(datos.head())
-
-user_input = st.text_input("label goes here", default_value_goes_here)
-
-query_emb = model.encode(query)
-
-sims = util.cos_sim(query_emb, emb)
-sims = [(float(s),i) for i, s in enumerate(sims[0])]
-sims.sort(reverse=True)
-for s, i in sims[:10]:
-    st.write(str(s)+"\t"+labs[i]+": "+ text[i])
+#---------------------------------------------------------#
+if query is not None:
+    query_emb = model.encode(query)
     
-Counter(labs[i] for _,i in sims[:100])
+    sims = util.cos_sim(query_emb, emb)
+    sims = [(float(s),i) for i, s in enumerate(sims[0])]
+    sims.sort(reverse=True)
+    
+    for s, i in sims[:10]:
+        st.write(str(s)+"\t"+labs[i]+": "+ text[i])
+    Counter(labs[i] for _,i in sims[:100])
+    
+else:
+    st.write("falta query")
+
+
+#---------------------------------------------------------#
