6725f87b106e0b772116c59a4e915e042a3f3a4c
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-18 21:25:59 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -110,7 +110,7 @@ data1 = pd.DataFrame()
 for s, i in sims[:10]:
     #st.write(str(s)+"\t"+labs[i]+": "+ text[i])  
     data = [str(s), labs[i], text[i]]
-    data1 = data1.append(data)
+    data1 = data1.append(data, axis = 1)
 st.table(data1)
     
 st.markdown("""---""")
