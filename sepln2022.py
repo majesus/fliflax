e6dcafd09d3714c80e7a5858f36c0de96c9e98e2
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-19 00:58:28 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -143,7 +143,7 @@ st.title('Estimación de sentimientos:')
 st.write('Esta app-streamlit emplea Hugging Face Transformers [sentiment analyser](https://huggingface.co/course/chapter1/3?fw=tf) para clasificar el texto como positivo o negativo.')
 
 form = st.form(key='sentiment-form')
-user_input = form.text_area('texto')
+user_input = form.text_area('texto', value = target)
 submit = form.form_submit_button('enviar')
 
 if submit:
