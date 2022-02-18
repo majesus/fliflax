01cd3e87986dfd0b82580a95f520bf0167046172
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-19 00:00:42 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -110,7 +110,7 @@ for s, i in sims[:10]:
     dat= [str(s), labs[i], text[i]]
     L.append(dat)
 df = pd.DataFrame(L, columns = ['cosine', 'type', 'string'])
-st.dataframe(df.style.format("{:.4%}"))
+st.table(df.style.format({"cosine": "{:.2f}"}))
     
 st.markdown("""---""")
 
