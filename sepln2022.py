4f1869a431d911e8cb169656888e47ba1ea8a8fd
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-19 11:04:47 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -69,7 +69,7 @@ with st.sidebar.form(key='my_form'):
     masking = st.radio("¿masking?", (True,False))
     material = st.radio("¿material?", ("frases","revisiones"))
     n_top = st.slider(label='número de materiales a emplear por tipo de alojamiento:', value=10, max_value= 1000, min_value = 1)
-    target = st.text_input(label='query a comparar su similitud [coseno] con dataset:', value = "I am satisfied with this stay.")
+    target = st.text_input(label='query a comparar su similitud [coseno] con material:', value = "I am satisfied with this stay.")
     form1 = st.form_submit_button(label='Calcular')
 #---------------------------------------------------------#
 datos = datos.groupby('type').apply(lambda x: x.sample(n=n_top, replace = False, random_state=123)).reset_index(drop = True)
