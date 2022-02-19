e71b900277f2aeffe2b42e2341ee7c6b2970e4d8
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-19 01:50:35 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -127,7 +127,7 @@ df = pd.DataFrame(L, columns = ['cosine', 'type', 'string'])
 
 st.success("Materiales ordenador de mayor a menor similitud con el texto con que comparamos:")
 df1 = df
-df1.set_index('airbnb', inplace=True)
+df1.set_index('type', inplace=True)
 st.table(df1.head())
     
 st.markdown("""---""")
