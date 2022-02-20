3b035b16b2b3d7892466fcfce65cc4574df18f5d
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-20 11:31:47 +0100

    Update zero_shot.py

--- zero_shot.py
+++ zero_shot.py
@@ -11,17 +11,18 @@ datos = pd.read_csv("csv/proyecto.csv")
 
 with st.sidebar.form(key='my_form'):
     options = st.multiselect('What are your favorite colors',['location', 'price'])
+    n_top = st.slider(label='número de revisiones a emplear por tipo de alojamiento:', value=10, max_value= 100, min_value = 1)
     form1 = st.form_submit_button(label='Calcular')
 #---------------------------------------------------------#
-datos = datos.groupby('type').apply(lambda x: x.sample(n=n_top, replace = False, random_state=123)).reset_index(drop = True)
+datos_zero = datos.groupby('type').apply(lambda x: x.sample(n=n_top, replace = False, random_state=123)).reset_index(drop = True)
 #---------------------------------------------------------#
 #---------------------------------------------------------#
 st.markdown("""---""")
 #---------------------------------------------------------#
 candidate_labels = ["location", "price"]
 candidate_results = [0, 0]
-datos1 = datos.head(10)
-for sent in datos1['description1'].values:
+
+for sent in datos_zero['description1'].values:
     # To do multi-class classification, simply pass multi_class=True.
     # In this case, the scores will be independent, but each will fall between 0 and 1.
     res = classifier(sent, candidate_labels)
