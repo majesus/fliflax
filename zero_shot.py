0be2b7a96f2eb0dc4129fa4313be8a0cebc7619c
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-20 11:30:30 +0100

    Update zero_shot.py

--- zero_shot.py
+++ zero_shot.py
@@ -10,7 +10,7 @@ datos = pd.read_csv("csv/proyecto.csv")
 # Decide si se enmascaran palabras o no
 
 with st.sidebar.form(key='my_form'):
-    options = st.multiselect('What are your favorite colors',['location', 'price'],['satisfaction', 'trust'])
+    options = st.multiselect('What are your favorite colors',['location', 'price'])
     form1 = st.form_submit_button(label='Calcular')
 #---------------------------------------------------------#
 datos = datos.groupby('type').apply(lambda x: x.sample(n=n_top, replace = False, random_state=123)).reset_index(drop = True)
@@ -18,41 +18,32 @@ datos = datos.groupby('type').apply(lambda x: x.sample(n=n_top, replace = False,
 #---------------------------------------------------------#
 st.markdown("""---""")
 #---------------------------------------------------------#
-submit_zero = form.form_submit_button('Calcular')
-if form1:
-    classifier = pipeline("zero-shot-classification")
-
-    candidate_labels = ["location", "price"]
+candidate_labels = ["location", "price"]
+candidate_results = [0, 0]
+datos1 = datos.head(10)
+for sent in datos1['description1'].values:
+    # To do multi-class classification, simply pass multi_class=True.
+    # In this case, the scores will be independent, but each will fall between 0 and 1.
     res = classifier(sent, candidate_labels)
 
-    datos_head = datos.head(n_top)
-    for sent in datos_head['description1'].values:
-        # To do multi-class classification, simply pass multi_class=True.
-        # In this case, the scores will be independent, but each will fall between 0 and 1.
-        res = classifier(sent, candidate_labels)
+    SCORES = res["scores"]
+    CLASSES = res["labels"]
+    BEST_INDEX = argmax(SCORES)
+    predicted_class = CLASSES[BEST_INDEX]
+    predicted_score = SCORES[BEST_INDEX]
 
-        SCORES = res["scores"]
-        CLASSES = res["labels"]
-        BEST_INDEX = argmax(SCORES)
-        predicted_class = CLASSES[BEST_INDEX]
-        predicted_score = SCORES[BEST_INDEX]
+    if predicted_class == 'renewable' and predicted_score > 0.5:
+        candidate_results[0] = candidate_results[0] + 1
+    if predicted_class == 'politics' and predicted_score > 0.5:
+        candidate_results[1] = candidate_results[1] + 1
+    
+    if res['scores'][0] > 0.5:
+        print(sent)
+        print(res['labels'])
+        print(res['scores'])
+        print()
 
-        if predicted_class == 'location' and predicted_score > 0.5:
-            candidate_results[0] = candidate_results[0] + 1
-        if predicted_class == 'price' and predicted_score > 0.5:
-            candidate_results[1] = candidate_results[1] + 1
-
-        if res['scores'][0] > 0.5:
-            print(sent)
-            print(res['labels'])
-            print(res['scores'])
-            print()
-
-    st.write(candidate_results)
-
-    data = {'labels': candidate_labels,'values': candidate_results}
-    df_chart = pd.DataFrame(data, columns=['labels','values'])
-    st.table(df_chart.head(10))
+st.wirte(candidate_results)
 #---------------------------------------------------------#
 
 
