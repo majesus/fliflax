7149ea728ad78a9beeef33013c143f3c0b1c65f9
Manuel J. Sánchez-Franco <majesus@us.es>
2022-02-20 10:22:43 +0100

    Update sepln2022.py

--- sepln2022.py
+++ sepln2022.py
@@ -216,3 +216,51 @@ if submit:
 #---------------------------------------------------------#
 
 #---------------------------------------------------------#
+classifier = pipeline("zero-shot-classification", device=0)
+
+candidate_labels = ["location", "price", "relationship", "trust", "recommendation", "satisfaction"]
+res = classifier(sent, candidate_labels)
+
+datos_head = datos.head(n_top)
+for sent in datos_head['description1'].values:
+    # To do multi-class classification, simply pass multi_class=True.
+    # In this case, the scores will be independent, but each will fall between 0 and 1.
+    res = classifier(sent, candidate_labels)
+
+    SCORES = res["scores"]
+    CLASSES = res["labels"]
+    BEST_INDEX = argmax(SCORES)
+    predicted_class = CLASSES[BEST_INDEX]
+    predicted_score = SCORES[BEST_INDEX]
+
+    if predicted_class == 'location' and predicted_score > 0.5:
+        candidate_results[0] = candidate_results[0] + 1
+    if predicted_class == 'price' and predicted_score > 0.5:
+        candidate_results[1] = candidate_results[1] + 1
+    if predicted_class == 'relationship' and predicted_score > 0.5:
+        candidate_results[2] = candidate_results[2] + 1
+    if predicted_class == 'trust' and predicted_score > 0.5:
+        candidate_results[3] = candidate_results[3] + 1
+    if predicted_class == 'recommendation' and predicted_score > 0.5:
+        candidate_results[4] = candidate_results[4] + 1
+    if predicted_class == 'satisfaction' and predicted_score > 0.5:
+        candidate_results[5] = candidate_results[5] + 1
+
+    if res['scores'][0] > 0.5:
+        print(sent)
+        print(res['labels'])
+        print(res['scores'])
+        print()
+
+st.write(candidate_results)
+
+data = {'labels': candidate_labels,
+        'values': candidate_results}
+df_chart = pd.DataFrame(data, columns=['labels','values'])
+st.table(df_chart.head(10))
+#---------------------------------------------------------#
+
+
+
+
+
