8dfa7c9f84c887fbbda470b0599f594689c029a5
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 08:04:13 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -5,6 +5,8 @@ import streamlit as st
 import altair as alt
 import snscrape.modules.twitter as sntwitter
 import itertools
+
+import twint
 #-----------------------------------------------------------------#
 from PIL import Image
 img=Image.open('img/fliflax-logo.jpg')
@@ -57,6 +59,28 @@ with st.sidebar.form("my_form"):
     file_name = st.text_input('Nombre del CSV:')
       
     submitted = st.form_submit_button(label = "Descargar")
+#-----------------------------------------------------------------#
+#-----------------------------------------------------------------#
+with st.form(key=’Twitter_form’):
+      search_term = st.text_input(‘What do you want to search for?’)
+      limit = st.slider(‘How many tweets do you want to get?’, 
+                         min, 
+                         max, 
+                         step=int)
+      output_csv = st.radio(‘Save a CSV file?’, 
+                             [‘Yes’, ‘No’])
+      file_name = st.text_input(‘Name the CSV file:’)
+      submit_button = st.form_submit_button(label=’Search’)
+if submit_button:
+        c = twint.Config()
+        c.Search = search_term
+        c.Limit = limit
+        c.Store_csv = True
+        if c.Store_csv:
+            c.Output = f'{file_name}.csv'
+        twint.run.Search(c)
+data = pd.read_csv(f'{file_name}.csv', usecols=['date','tweet'])
+st.table(data)
 #-----------------------------------------------------------------#
 #-----------------------------------------------------------------#
 
