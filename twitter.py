1ae02cc8e2de2d2266a9da9aaf448fbf56b033a3
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 21:28:11 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -34,7 +34,7 @@ st.markdown("----")
 # customize form
 with st.sidebar.form(key='Twitter_form'):
     #search_term = st.text_input('¿Qué deseas buscar?', value = "unisevilla")
-    search_term = st.selectbox("¿Qué deseas buscar?", ('unisevilla','zara_es','RealBetis','SevillaFC'))
+    search_term = st.selectbox("¿Qué deseas buscar?", ('unisevilla','zara_es','RealBetis','SevillaFC','DonaldTrump','JoeBiden))
     limit = st.slider('¿Cuántos tweets deseas descargar?', 20, 200, step=20)
     
     #output_csv = st.radio('Save a CSV file?', ['Si', 'No'])
