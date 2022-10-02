c4373d9181c0d974e45aaa1cc57ae3edd8681d98
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 08:42:03 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -38,7 +38,10 @@ sentiments = []
 # customize form
 with st.form(key='Twitter_form'):
     search_term = st.text_input('What do you want to search for?', value = "deporte")
-    limit = st.slider('How many tweets do you want to get?', 0, 500, step=20)
+    limit = st.slider('How many tweets do you want to get?', 0, 100, step=5)
+    desde_fecha = st.date_input('¿Desde qué fecha?',value = dt.datetime.now(), key ="date_min")
+    hasta_fecha = st.date_input('¿Hasta qué fecha?',value = dt.datetime.now(), key ="date_max")
+    
     output_csv = st.radio('Save a CSV file?', ['Yes', 'No'])
     file_name = st.text_input('Name the CSV file:')
     submit_button = st.form_submit_button(label='Search')
@@ -57,7 +60,7 @@ with st.form(key='Twitter_form'):
 
         twint.run.Search(c)
 
-        data = pd.read_csv(f'{file_name}.csv', usecols=['date', 'tweet'])
+        data = pd.read_csv(f'{file_name}.csv', usecols=['date', 'tweet', 'replies_count', 'retweets_count', 'likes_count'])
 
         st.table(data)
 
