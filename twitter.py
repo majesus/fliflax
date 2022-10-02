f745291ac370558407830a81e3d503039db1e1cc
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 11:41:33 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -43,6 +43,8 @@ with st.sidebar.form(key='Twitter_form'):
     output_csv = st.radio('Save a CSV file?', [True, False])
     file_name = st.text_input('Name the CSV file:', value = search_term)
     submit_button = st.form_submit_button(label='Search')
+    
+st.write(f'{output_csv})
 
 if submit_button:
   # configure twint
