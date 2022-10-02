f66165d0e4305f9b4d741f41ae1a07f1f9f06f7b
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 11:59:15 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -58,7 +58,7 @@ if submit_button:
   if c.Store_csv != output_csv:
     c.Custom_csv = ['date', 'tweet', 'replies_count', 'retweets_count', 'likes_count']
     c.Output = f'{file_name}.csv'
-    #data = pd.read_csv(f'{file_name}.csv')
+    data = pd.read_csv(f'{file_name}.csv')
     st.write('CSV ', output_csv)
     #st.table(data)
   elif c.Store_csv == output_csv:
