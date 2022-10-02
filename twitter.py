9c7f0e5326759babcb0de06b227e0723e6b51113
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 12:49:00 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -66,3 +66,14 @@ if submit_button:
   
   st.table(Tweets_df)
   
+@st.cache
+def convert_df(df):
+   return df.to_csv().encode('utf-8')
+csv = convert_df(Tweets_df)
+st.download_button(
+   "Press to Download",
+   csv,
+   "file.csv",
+   "text/csv",
+   key='download-csv'
+)
