a39783d47ef65b8f4ef89d601e44ee8707e07190
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 12:02:46 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -54,8 +54,9 @@ if submit_button:
   c.Limit = limit
   c.Pandas = True
   twint.run.Search(c)
+  c.Store_csv = output_csv
 
-  if c.Store_csv == output_csv:
+  if c.Store_csv:
     c.Custom_csv = ['date', 'tweet', 'replies_count', 'retweets_count', 'likes_count']
     c.Output = f'{file_name}.csv'
     data = pd.read_csv(f'{file_name}.csv')
@@ -63,7 +64,7 @@ if submit_button:
     st.write('CSV ', output_csv)
     st.write('Number of rows ', len_df)
     #st.table(data)
-  elif c.Store_csv != output_csv:
+  else:
     data = pd.DataFrame()
     data = twint.storage.panda.Tweets_df
     st.write('CSV ', output_csv)
