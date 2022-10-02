6e8e8063749cc7a6bf520d301168b29a9b534a5b
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 11:26:11 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -55,9 +55,9 @@ if submit_button:
 
   c.Store_csv = True
   if c.Store_csv:
-    # c.Custom_csv = ['date', 'tweet', 'replies_count', 'retweets_count', 'likes_count']
+    c.Custom_csv = ['date', 'tweet', 'replies_count', 'retweets_count', 'likes_count']
     c.Output = f'{file_name}.csv'
     data = pd.read_csv(f'{file_name}.csv')
     Tweets_df = twint.storage.panda.Tweets_df
 
-    st.table(Tweets_df)
+    st.table(data)
