a5a1f9c42746744dae448f1bd31cdfc6751fb72e
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 11:39:47 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -59,5 +59,6 @@ if submit_button:
     data = pd.read_csv(f'{file_name}.csv')
     st.table(data)
   else:
+    Tweets_df = pd.DataFrame(index=index, columns=columns)
     Tweets_df = twint.storage.panda.Tweets_df
     st.table(Tweets_df)
