42c188671ec4d25daf9a5df278620b07d09c7eea
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 11:30:51 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -41,7 +41,7 @@ with st.sidebar.form(key='Twitter_form'):
     limit = st.slider('How many tweets do you want to get?', 0, 100, step=20)
     
     output_csv = st.radio('Save a CSV file?', ['No', 'Si'])
-    file_name = st.text_input('Name the CSV file:')
+    file_name = st.text_input('Name the CSV file:', value = search_term)
     submit_button = st.form_submit_button(label='Search')
 
 if submit_button:
