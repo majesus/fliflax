f6c8c07e05011db9284979094b2c20a806c686e1
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 18:46:09 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -57,7 +57,7 @@ if submit_button:
   Tweets_df = pd.DataFrame()
   Tweets_df = pd.read_csv(f'{file_name}.csv')
   Tweets_df = Tweets_df[['date', 'tweet', 'replies_count', 'retweets_count', 'likes_count']]
-  Tweets_df = Tweets_df.iloc[0:limit-1] 
+  Tweets_df = Tweets_df.iloc[0:limit] 
   
   len_df = len(Tweets_df.index)
   st.write('Número de tweets ', len_df)
