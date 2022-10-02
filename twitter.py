a9adcc8bf918a341626034d0faf5d54251756f0a
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 19:18:09 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -38,8 +38,8 @@ with st.sidebar.form(key='Twitter_form'):
     #output_csv = st.radio('Save a CSV file?', ['Si', 'No'])
     #desde_fecha = st.date_input('¿Desde qué fecha?',value = dt.datetime.now(), key ="date_min")
     
-    file_name = ''.join(random.choices(string.ascii_uppercase, k = 8))  
-    file_name = st.text_input('Name the CSV file:', value = file_name)
+    #file_name = ''.join(random.choices(string.ascii_uppercase, k = 8))  
+    file_name = st.text_input('Name the CSV file:', value = search_term)
     
     st.write('filename',file_name)
     submit_button = st.form_submit_button(label='Search')
