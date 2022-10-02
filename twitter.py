b7d5ed9453aef346767a6d9b08a33ab674dff9e2
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 08:08:53 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -34,23 +34,19 @@ st.markdown("----")
 import datetime as dt
 #-----------------------------------------------------------------#
 with st.form(key='Twitter_form'):
-      search_term = st.text_input('What do you want to search for?')
-      limit = st.slider('How many tweets do you want to get?', 
-                         min, 
-                         max, 
-                         step=int)
-      output_csv = st.radio('Save a CSV file?', 
-                             ['Yes', 'No'])
-      file_name = st.text_input('Name the CSV file:')
-      submit_button = st.form_submit_button(label='Search')
+  search_term = st.text_input('What do you want to search for?')
+  limit = st.slider('How many tweets do you want to get?', min, max, step=int)
+  output_csv = st.radio('Save a CSV file?', ['Yes', 'No'])
+  file_name = st.text_input('Name the CSV file:')
+  submit_button = st.form_submit_button(label='Search')
 if submit_button:
-        c = twint.Config()
-        c.Search = search_term
-        c.Limit = limit
-        c.Store_csv = True
-        if c.Store_csv:
-            c.Output = f'{file_name}.csv'
-        twint.run.Search(c)
+  c = twint.Config()
+  c.Search = search_term
+  c.Limit = limit
+  c.Store_csv = True
+  if c.Store_csv:
+    c.Output = f'{file_name}.csv'
+  twint.run.Search(c)
 data = pd.read_csv(f'{file_name}.csv', usecols=['date','tweet'])
 st.table(data)
 #-----------------------------------------------------------------#
