a386a8525906ae833e08644d7dfeb45f86c70a23
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-02 08:33:39 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -31,25 +31,58 @@ st.markdown("----")
 #-----------------------------------------------------------------#
 
 #-----------------------------------------------------------------#
-import datetime as dt
-#-----------------------------------------------------------------#
+
+languages = []
+sentiments = []
+
+# customize form
 with st.form(key='Twitter_form'):
-  search_term = st.text_input('What do you want to search for?', value = 'zara_es')
-  limit = st.slider('How many tweets do you want to get?', min_value=1, max_value=100, value=2, step=10)
-  output_csv = st.radio('Save a CSV file?', ['Yes', 'No'])
-  file_name = st.text_input('Name the CSV file:')
-  submit_button = st.form_submit_button(label='Search')
-if submit_button:
-  # Configure
-  c = twint.Config()
-  c.Limit = 1
-  c.Username = search_term
-  c.Pandas = True
-  # Run
-  twint.run.Search(c)
-  Tweets_df = twint.storage.panda.Tweets_df
-  st.table(Tweets_df)
-  
+    search_term = st.text_input('What do you want to search for?')
+    limit = st.slider('How many tweets do you want to get?', 0, 500, step=20)
+    output_csv = st.radio('Save a CSV file?', ['Yes', 'No'])
+    file_name = st.text_input('Name the CSV file:')
+    submit_button = st.form_submit_button(label='Search')
 
-#-----------------------------------------------------------------#
+    if submit_button:
+        # configure twint
+        c = twint.Config()
 
+        c.Search = search_term
+        c.Limit = limit
+
+        c.Store_csv = True
+
+        if c.Store_csv:
+            c.Output = f'{file_name}.csv'
+
+        twint.run.Search(c)
+
+        data = pd.read_csv(f'{file_name}.csv', usecols=['date', 'tweet'])
+
+        for x in data['tweet']:
+            # Get language of the tweet
+            lang = comprehend.detect_dominant_language(Text=x)['Languages'][0]['LanguageCode']
+            languages.append(lang)
+            print(f'Language detected: {lang}')
+        data['languages']=languages
+
+            # Get the sentiment of the tweet
+        for x in range(len(data['tweet'])):
+            try:
+                sent = comprehend.detect_sentiment(
+                                                    Text=data['tweet'][x],
+                                                    LanguageCode= data['languages'][x]
+                                                    )['Sentiment']
+                sentiments.append(sent)
+                print(f'Sentiment detected:{sent}')
+            except botocore.exceptions.ClientError as error:
+                print(f'Error: {error}')
+                sentiments.append(error)
+        data['sentiment']= sentiments
+        st.table(data)
+
+
+try:
+    st.download_button(label='Download results', data=convert_df(data), file_name = f'{file_name}.csv', mime='text/csv')
+except:
+    pass
