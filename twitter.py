1bfb1ad4cc5103eeb2f43bb76e4243db37fe66f2
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-03 19:21:32 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -38,10 +38,11 @@ with st.sidebar.form(key='Twitter_form'):
     search_term = st.selectbox("¿Qué deseas buscar?", ('JoeBiden','RealBetis','SevillaFC','zara_es'))
     limit = st.slider('¿Cuántos tweets deseas descargar?', 20, 200, step=20)
     
-    last_24_date_time = datetime.now() - timedelta(days = 7)
+    last_24_date_time = st.date_input('¿Desde qué fecha?',value = datetime.now() - timedelta(days = 7), key ="date_min")
+    #last_24_date_time = datetime.now() - timedelta(days = 7)
     today = last_24_date_time.strftime('%Y-%m-%d')
     date = str(today)
-    
+        
     file_name = ''.join(random.choices(string.ascii_uppercase, k = 10))  
     file_name = st.text_input('Nombre del CSV:', value = file_name)
 
