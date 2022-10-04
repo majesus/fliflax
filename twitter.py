1520b50208060851053fde5e877c3f8739bd0e1e
Manuel J. Sánchez-Franco <majesus@us.es>
2022-10-04 08:17:17 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -39,6 +39,8 @@ with st.sidebar.form(key='Twitter_form'):
     search_term = st.text_input('¿Qué deseas buscar? Indica una cuenta de Twitter', value = "")
     limit = st.slider('¿Cuántos tweets deseas descargar?', 20, 200, step=20)
     
+    retweets = st.radio('¿Deseas descargar los retweets?', [True, False])
+    
     last_24_date_time = st.date_input('¿Desde qué fecha?',value = datetime.now() - timedelta(days = 7), key ="date_min")
     #last_24_date_time = datetime.now() - timedelta(days = 7)
     today = last_24_date_time.strftime('%Y-%m-%d')
@@ -55,7 +57,7 @@ if submit_button:
   c.Since = date
    
   c.Verified = True
-  c.Retweets = False
+  c.Retweets = retweets
   c.Filter_retweets = False 
   c.Hide_output = False
   c.Lang = 'es'
