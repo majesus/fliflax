67c9af953e2e05c7e86d076ae23755b5f78256e4
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-19 21:36:25 +0100

    Update PAP.py

--- PAP.py
+++ PAP.py
@@ -51,6 +51,20 @@ st.write("La investigación es un pilar fundamental en nuestro departamento. Par
 st.write("Invitamos a los interesados a unirse a nuestra comunidad académica, donde encontrarán un entorno de "
          "aprendizaje y crecimiento, apoyado por **profesionales dedicados a la enseñanza y la investigación**.")
 #----------------------------------------------------#
+from streamlit_echarts import st_echarts
+
+options = {
+    "xAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "yAxis": {"type": "value"},
+    "series": [
+        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
+    ],
+}
+st_echarts(options=options)
+#----------------------------------------------------#
 
 import streamlit as st
 from streamlit_echarts import st_echarts
