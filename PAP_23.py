5def2b0d56e6837f3866ff9669660af5d269efda
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-20 21:19:23 +0100

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -37,19 +37,24 @@ elif menu == "Personal":
     st.markdown("Lista del personal académico y administrativo, roles y áreas de especialización.")
 
     dfg = pd.read_csv('csv/investigadores.csv', sep=",")
-  
+
     dfg1 = dfg.set_index('Área de Conocimiento')
     selected_indices = st.multiselect('Selecciona el área de conocimiento:', dfg1.index.unique())
-    selected_indices = map(lambda selected_indices:selected_indices, selected_indices)
-    selected_rows = dfg1.loc[selected_indices]
 
-    # Convierte el nombre en un enlace HTML que apunta a la URL correspondiente
-    selected_rows["Nombre"] = selected_rows.apply(lambda row: f'<a href="{row["URL"]}" target="_blank">{row["Nombre"]}</a>', axis=1)
-    # Muestra el DataFrame en Streamlit como una tabla HTML
-    selected_rows = selected_rows.drop(['Departamento', 'URL'], axis=1)
-    #st.write(selected_rows.to_html(escape=False, index=False), unsafe_allow_html=True)
-    st.write(f'<div style="font-size: 10px;">{selected_rows.to_html(escape=False, index=False)}</div>', unsafe_allow_html=True)
+    if selected_indices:
+        selected_indices = map(lambda selected_indices:selected_indices, selected_indices)
+        selected_rows = dfg1.loc[selected_indices]
 
+        # Convierte el nombre en un enlace HTML que apunta a la URL correspondiente
+        selected_rows["Nombre"] = selected_rows.apply(lambda row: f'<a href="{row["URL"]}" target="_blank">{row["Nombre"]}</a>', axis=1)
+        # Muestra el DataFrame en Streamlit como una tabla HTML
+        selected_rows = selected_rows.drop(['Departamento', 'URL'], axis=1)
+        #st.write(selected_rows.to_html(escape=False, index=False), unsafe_allow_html=True)
+        st.write(f'<div style="font-size: 10px;">{selected_rows.to_html(escape=False, index=False)}</div>', unsafe_allow_html=True)
+    else:
+        st.write("Selecciona al menos un área de conocimiento para ver la tabla.")
+
+   
 # Contacto
 elif menu == "Contacto":
     st.subheader("Contacto")
