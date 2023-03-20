ed83681fa2ae4496ebc45565113d57cf1e2b7875
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-20 21:16:29 +0100

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -47,7 +47,8 @@ elif menu == "Personal":
     selected_rows["Nombre"] = selected_rows.apply(lambda row: f'<a href="{row["URL"]}" target="_blank">{row["Nombre"]}</a>', axis=1)
     # Muestra el DataFrame en Streamlit como una tabla HTML
     selected_rows = selected_rows.drop(['Departamento', 'URL'], axis=1)
-    st.write(selected_rows.to_html(escape=False, index=False), unsafe_allow_html=True)
+    #st.write(selected_rows.to_html(escape=False, index=False), unsafe_allow_html=True)
+    st.write(f'<div style="font-size: 10px;">{selected_rows.to_html(escape=False, index=False)}</div>', unsafe_allow_html=True)
 
 # Contacto
 elif menu == "Contacto":
