edb977ff6721d27c9b3825047c48fa9b46741825
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-20 08:31:06 +0100

    Update PAP.py

--- PAP.py
+++ PAP.py
@@ -1,54 +1,4 @@
 import streamlit as st
-
-# Encabezado
-st.set_page_config(page_title="Departamento de Administración de Empresas y Marketing", page_icon=":mortar_board:")
-st.image("img/fliflax-logo.jpg", width=200)
-st.title("Departamento de Administración de Empresas y Marketing")
-st.markdown("---")
-
-# Menú de navegación
-menu = st.sidebar.selectbox("Menú de navegación", ("Inicio", "Acerca de", "Investigación", "Docencia", "Personal", "Contacto"))
-
-# Inicio
-if menu == "Inicio":
-    st.subheader("Bienvenidos")
-    st.markdown("Texto de bienvenida y descripción del departamento.")
-    st.image("img/fliflax-logo.jpg", caption="Imagen destacada")
-
-# Acerca de
-elif menu == "Acerca de":
-    st.subheader("Acerca de")
-    st.markdown("Historia, misión y objetivos del departamento.")
-
-# Investigación
-elif menu == "Investigación":
-    st.subheader("Investigación")
-    st.markdown("Áreas de investigación, proyectos, publicaciones y colaboraciones.")
-
-# Docencia
-elif menu == "Docencia":
-    st.subheader("Docencia")
-    st.markdown("Programas académicos, cursos, horarios y recursos para estudiantes.")
-
-# Personal
-elif menu == "Personal":
-    st.subheader("Personal")
-    st.markdown("Lista del personal académico y administrativo, roles y áreas de especialización.")
-
-# Contacto
-elif menu == "Contacto":
-    st.subheader("Contacto")
-    st.markdown("Información de contacto del departamento.")
-    st.markdown("Formulario de contacto (puedes usar el componente `st.form` para crear un formulario).")
-
-
-
-
-
-
-
-
-import streamlit as st
 #----------------------------------------------------#
 from PIL import Image
 img=Image.open('img/fliflax-logo.jpg')
@@ -84,6 +34,54 @@ hide_menu_style = """
         """
 #st.markdown(hide_menu_style, unsafe_allow_html=True)
 #----------------------------------------------------#
+
+
+
+import streamlit as st
+
+# Encabezado
+st.set_page_config(page_title="Departamento de Administración de Empresas y Marketing", page_icon=":mortar_board:")
+st.image("img/fliflax-logo.jpg", width=200)
+st.title("Departamento de Administración de Empresas y Marketing")
+st.markdown("---")
+
+# Menú de navegación
+menu = st.sidebar.selectbox("Menú de navegación", ("Inicio", "Acerca de", "Investigación", "Docencia", "Personal", "Contacto"))
+
+# Inicio
+if menu == "Inicio":
+    st.subheader("Bienvenidos")
+    st.markdown("Texto de bienvenida y descripción del departamento.")
+    #st.image("img/fliflax-logo.jpg", caption="Imagen destacada")
+
+# Acerca de
+elif menu == "Acerca de":
+    st.subheader("Acerca de")
+    st.markdown("Historia, misión y objetivos del departamento.")
+
+# Investigación
+elif menu == "Investigación":
+    st.subheader("Investigación")
+    st.markdown("Áreas de investigación, proyectos, publicaciones y colaboraciones.")
+
+# Docencia
+elif menu == "Docencia":
+    st.subheader("Docencia")
+    st.markdown("Programas académicos, cursos, horarios y recursos para estudiantes.")
+
+# Personal
+elif menu == "Personal":
+    st.subheader("Personal")
+    st.markdown("Lista del personal académico y administrativo, roles y áreas de especialización.")
+
+# Contacto
+elif menu == "Contacto":
+    st.subheader("Contacto")
+    st.markdown("Información de contacto del departamento.")
+    st.markdown("Formulario de contacto (puedes usar el componente `st.form` para crear un formulario).")
+
+
+
 #----------------------------------------------------#
 st.image('img/fliflax-logo.jpg',width=200)
 st.title("Fliflax: Una plataforma de apoyo al estudio")
