a71c113ece6c8a35b8fd9c44203bc76b2e6da4ae
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-22 00:00:28 +0100

    Update PAP_scraping.py

--- PAP_scraping.py
+++ PAP_scraping.py
@@ -37,8 +37,7 @@ df["Nombre"] = df.apply(lambda row: f'<a href="{row["URL_abs"]}" target="_blank"
 
 # Muestra el DataFrame en Streamlit como una tabla HTML
 # st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
-# st.write(f'<div style="font-size: 12;">{df.to_html(escape=False, index=False)}</div>', unsafe_allow_html=True)
-st.dataframe(df, 800, 1800)
+# st.write(f'<div style="font-size: 11px;">{df_csv.to_html(escape=False, index=False)}</div>', unsafe_allow_html=True)
 
 # Función para descargar el DataFrame como un archivo CSV
 def to_csv_download_link(df, filename):
