c1662d61b277aac54088e28c3e39aa2b0b8eed30
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-22 01:17:12 +0100

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -44,11 +44,11 @@ elif menu == "Investigación":
 
     # Selector de profesores
     # selected_professor = st.selectbox("Selecciona un profesor:", df_result["Nombre"])
-    df_result = df_result.set_index('Nombre')
+    # df_result = df_result.set_index('Nombre')
     selected_professor = st.multiselect('Selecciona el nombre del profesor:', df_result.index.unique())
 
     # Muestra la ficha del profesor seleccionado
-    selected_professor = map(lambda selected_professor:selected_professor, selected_professor)
+    # selected_professor = map(lambda selected_professor:selected_professor, selected_professor)
     #professor_data = df_result.loc[selected_professor]
     professor_data = df_result[df_result["Nombre"] == selected_professor].squeeze()
 
