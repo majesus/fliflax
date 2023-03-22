41a30f507d3dce9fa4bf7ec265faeeb1d73fd559
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-22 12:48:28 +0100

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -31,13 +31,31 @@ elif menu == "Investigación":
     st.subheader("Investigación")
     st.markdown("Áreas de investigación, proyectos, publicaciones y colaboraciones.")
 
+    dfg = pd.read_csv('csv/investigadores.csv', sep=",")
+
+    dfg1 = dfg.set_index('Área de Conocimiento')
+    selected_indices = st.multiselect('Selecciona al menos un área de conocimiento:', dfg1.index.unique())
+
+    if selected_indices:
+        selected_indices = map(lambda selected_indices:selected_indices, selected_indices)
+        selected_rows = dfg1.loc[selected_indices]
+
+        # Convierte el nombre en un enlace HTML que apunta a la URL correspondiente
+        selected_rows["Nombre"] = selected_rows.apply(lambda row: f'<a href="{row["URL"]}" target="_blank">{row["Nombre"]}</a>', axis=1)
+        # Muestra el DataFrame en Streamlit como una tabla HTML
+        selected_rows = selected_rows.drop(['Departamento', 'URL'], axis=1)
+        #st.write(selected_rows.to_html(escape=False, index=False), unsafe_allow_html=True)
+        st.write(f'<div style="font-size: 12px;">{selected_rows.to_html(escape=False, index=False)}</div>', unsafe_allow_html=True)
+    else:
+        st.write("Selecciona al menos un área de conocimiento para ver sus componentes.")
+    
 # Docencia
 elif menu == "Docencia":
     st.subheader("Docencia")
     st.markdown("Programas académicos, cursos, horarios y recursos para estudiantes.")
     
     # Diseño de la aplicación de Streamlit
-    st.title("Ficha del profesor")
+    st.title("Ficha del docente")
     
     # Lectura de la tabla con los datos de perfil:
     df_result0 = pd.read_csv('csv/profesores_perfil.csv', sep=",")
@@ -81,24 +99,6 @@ elif menu == "Personal":
     st.subheader("Personal")
     st.markdown("Lista del personal académico y administrativo, roles y áreas de especialización.")
 
-    dfg = pd.read_csv('csv/investigadores.csv', sep=",")
-
-    dfg1 = dfg.set_index('Área de Conocimiento')
-    selected_indices = st.multiselect('Selecciona el área de conocimiento:', dfg1.index.unique())
-
-    if selected_indices:
-        selected_indices = map(lambda selected_indices:selected_indices, selected_indices)
-        selected_rows = dfg1.loc[selected_indices]
-
-        # Convierte el nombre en un enlace HTML que apunta a la URL correspondiente
-        selected_rows["Nombre"] = selected_rows.apply(lambda row: f'<a href="{row["URL"]}" target="_blank">{row["Nombre"]}</a>', axis=1)
-        # Muestra el DataFrame en Streamlit como una tabla HTML
-        selected_rows = selected_rows.drop(['Departamento', 'URL'], axis=1)
-        #st.write(selected_rows.to_html(escape=False, index=False), unsafe_allow_html=True)
-        st.write(f'<div style="font-size: 12px;">{selected_rows.to_html(escape=False, index=False)}</div>', unsafe_allow_html=True)
-    else:
-        st.write("Selecciona al menos un área de conocimiento para ver la tabla.")
-   
 # Contacto
 elif menu == "Contacto":
     st.subheader("Contacto")
