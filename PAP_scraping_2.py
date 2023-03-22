41c7ecb0fecebb71d3a9412070c654c8a863f106
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-22 13:24:21 +0100

    Update PAP_scraping_2.py

--- PAP_scraping_2.py
+++ PAP_scraping_2.py
@@ -35,6 +35,9 @@ def obtener_info_investigador(url):
 
 def leer_urls_desde_csv(archivo_csv):
     df = pd.read_csv(archivo_csv)
+    
+    df = df.head()
+    
     print(df)  # Imprime el DataFrame leído desde el archivo CSV
     urls = df["url"].tolist()
     return urls
