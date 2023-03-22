ca1d479237fa3ff19bab31c3516ec3eebb683749
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-22 16:25:22 +0100

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -66,7 +66,7 @@ elif menu == "Investigación":
             st.write(f"**Grupo:** {professor_data['Grupo']}")
             st.write(f"**Instituto de Inv.:** {professor_data['Instituto de Inv.']}")
             st.write(f"**Prog. Doctorado:** {professor_data['Prog. Doctorado']}")
-            #st.write(f"**URL:** [{professor_data['URL']}]({professor_data['URL']})")
+            st.write(f"**URL:** [{professor_data['URL']}]({professor_data['URL']})")
 
     else:
         st.write("Selecciona un/a investigador/a.")
