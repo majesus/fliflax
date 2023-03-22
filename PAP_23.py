2852863de6610c2ab07ad879e4929525d85f8692
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-22 18:31:38 +0100

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -16,7 +16,7 @@ menu = option_menu(None, ["Acerca de", "Estudiantes", "Investigación", "Docenci
     styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "15px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "green"},
     }
 )
