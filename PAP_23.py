19f37b5009b1c7f666d1ea6683c73534b88b628d
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-27 12:02:47 +0200

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -198,7 +198,7 @@ elif menu == "Investigar":
     # Crear una lista utilizando la etiqueta <p> y enlazar cada grupo con su URL correspondiente
     # Crear una lista utilizando la etiqueta <p> y enlazar cada grupo con su URL correspondiente
     for index, row in grupos.iterrows():
-        st.markdown(f"<p style='{custom_style}' style='color: black; text-decoration: none;'><a href='{row['URL_grupo']}' target='_blank'>{row['Grupo']}</a></p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='{custom_style}', style='color: black; text-decoration: none;'><a href='{row['URL_grupo']}' target='_blank'>{row['Grupo']}</a></p>", unsafe_allow_html=True)
 
     separador("#B30A1B")
     
