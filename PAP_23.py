abe2e1d45d8678b9cf48c3c196f8ad35c4b65585
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-28 16:48:05 +0200

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -113,11 +113,11 @@ if menu == "Inicio":
     for item in equipo_directivo:
         st.markdown(f"<p style='{custom_style}'>{item}</p>", unsafe_allow_html=True)
 
-    st.markdown("----")
-    st.markdown(f"<p style='{custom_style}'>Facultad de Ciencias Económicas y Empresariales</p>", unsafe_allow_html=True)
-    st.markdown(f"<p style='{custom_style}'>Avda. Ramón y Cajal, n1, 41018, Sevilla, España</p>", unsafe_allow_html=True)
-    st.markdown(f"<p style='{custom_style}'>Teléfono: 954557575</p>", unsafe_allow_html=True)
-    st.markdown(f"<p style='{custom_style}'>Correo electrónico: empresa-us.es</p>", unsafe_allow_html=True)
+    with st.expander("Localización"):
+        st.markdown(f"<p style='{custom_style}'>Facultad de Ciencias Económicas y Empresariales</p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='{custom_style}'>Avda. Ramón y Cajal, n1, 41018, Sevilla, España</p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='{custom_style}'>Teléfono: 954557575</p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='{custom_style}'>Correo electrónico: empresa-us.es</p>", unsafe_allow_html=True)
         
     st.markdown(f"<p style='{custom_subtitle}'>Áreas de conocimiento</p>", unsafe_allow_html=True)
     for item in areas_conocimiento:
