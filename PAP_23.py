0af46f87cc1e0a960aa76a28d7a0185403fa4613
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-28 07:22:48 +0200

    Update PAP_23.py

--- PAP_23.py
+++ PAP_23.py
@@ -113,6 +113,12 @@ if menu == "Inicio":
     for item in equipo_directivo:
         st.markdown(f"<p style='{custom_style}'>{item}</p>", unsafe_allow_html=True)
 
+    st.markdown(f"<p style='{custom_title}'>CONTACTO</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='{custom_subtitle}'>Secretaría</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='{custom_style}'>Avda. Ramón y Cajal, n1, 41018, Sevilla, España</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='{custom_style}'>Teléfono: 954557575</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='{custom_style}'>Correo electrónico: empresa@us.es</p>", unsafe_allow_html=True)
+        
     st.markdown(f"<p style='{custom_subtitle}'>Áreas de conocimiento</p>", unsafe_allow_html=True)
     for item in areas_conocimiento:
         st.markdown(f"<p style='{custom_style}'>{item}</p>", unsafe_allow_html=True)
