59c762ea759bb660aad3536c712b5752e6d4f2fc
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-29 08:20:16 +0200

    Update graphs_research.py

--- graphs_research.py
+++ graphs_research.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 # Cargar tus datos desde el archivo CSV
 data = pd.read_csv("csv/dep_inv_prisma.txt")
@@ -9,10 +9,14 @@ st.write(data)
 # Calcular el número de publicaciones por año
 publications_by_year = data.groupby(['Año']).size().reset_index(name='Número de Publicaciones')
 
-# Crear el gráfico de área
-area_chart = px.area(publications_by_year, x='Año', y='Número de Publicaciones',
-                     title='Evolución del Número de Publicaciones por Año',
-                     range_x=[2000, 2023], width=1200)
+# Crear el gráfico de área suavizado
+area_chart = go.Figure()
+
+area_chart.add_trace(go.Scatter(x=publications_by_year['Año'], y=publications_by_year['Número de Publicaciones'],
+                                mode='lines', line_shape='spline', fill='tozeroy', line=dict(smoothing=0.5)))
+
+area_chart.update_layout(title='Evolución del Número de Publicaciones por Año',
+                         xaxis=dict(range=[2000, 2023]), width=1200)
 
 # Mostrar el gráfico en Streamlit
 st.plotly_chart(area_chart)
