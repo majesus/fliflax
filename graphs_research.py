2387e9f0caae7f21c252cc99f3ae96a0a65dee56
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-29 07:31:22 +0200

    Update graphs_research.py

--- graphs_research.py
+++ graphs_research.py
@@ -4,7 +4,7 @@ import altair as alt
 
 # Cargar tus datos desde el archivo CSV
 data = pd.read_csv("csv/dep_inv_prisma.txt")
-st.write(data.columns)
+st.write(data)
 
 # Calcular el número de publicaciones por año
 publications_by_year = data.groupby(['Año']).size().reset_index(name='Número de Publicaciones')
