e8b4af47ae59ef0d9fa6d71b3f7d1ddf426c02c4
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-29 08:34:48 +0200

    Update graphs_research.py

--- graphs_research.py
+++ graphs_research.py
@@ -2,20 +2,21 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-# Cargar tus datos desde el archivo CSV
-data = pd.read_csv("csv/dep_inv_prisma.txt")
+def mostrar_grafico_area_suavizado():
+  # Cargar tus datos desde el archivo CSV
+  data = pd.read_csv("csv/dep_inv_prisma.txt")
 
-# Calcular el número de publicaciones por año
-publications_by_year = data.groupby(['Año']).size().reset_index(name='Número de Publicaciones')
+  # Calcular el número de publicaciones por año
+  publications_by_year = data.groupby(['Año']).size().reset_index(name='Número de Publicaciones')
 
-# Crear el gráfico de área suavizado
-area_chart = go.Figure()
+  # Crear el gráfico de área suavizado
+  area_chart = go.Figure()
 
-area_chart.add_trace(go.Scatter(x=publications_by_year['Año'], y=publications_by_year['Número de Publicaciones'],
-                                mode='lines', line_shape='spline', fill='tozeroy', line=dict(smoothing=0.5)))
+  area_chart.add_trace(go.Scatter(x=publications_by_year['Año'], y=publications_by_year['Número de Publicaciones'],
+                                  mode='lines', line_shape='spline', fill='tozeroy', line=dict(smoothing=0.5)))
 
-area_chart.update_layout(title='Evolución del número de publicaciones por año [2000-2022]',
-                         xaxis=dict(range=[2000, 2022]), width=800)
+  area_chart.update_layout(title='Evolución del número de publicaciones por año [2000-2022]',
+                           xaxis=dict(range=[2000, 2022]), width=800)
 
-# Mostrar el gráfico en Streamlit
-st.plotly_chart(area_chart)
+  # Mostrar el gráfico en Streamlit
+  st.plotly_chart(area_chart)
