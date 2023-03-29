26acd91e6b8957a808db101b1cb53ca66da7f34f
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-29 07:58:57 +0200

    Update graphs_research.py

--- graphs_research.py
+++ graphs_research.py
@@ -16,7 +16,8 @@ area_chart = alt.Chart(publications_by_year).mark_area(
     alt.X('Año:Q', axis=alt.Axis(format='d'), scale=alt.Scale(domain=[2000, 2023])),
     alt.Y('Número de Publicaciones:Q', stack=None)
 ).properties(
-    title='Evolución del Número de Publicaciones por Año'
+    title='Evolución del Número de Publicaciones por Año',
+    width=1200
 )
 
 # Crear el gráfico de líneas
@@ -25,15 +26,12 @@ line_chart = alt.Chart(publications_by_year).mark_line(
 ).encode(
     alt.X('Año:Q', scale=alt.Scale(domain=[2000, 2023])),
     alt.Y('Número de Publicaciones:Q')
+).properties(
+    width=1200
 )
 
 # Combinar los dos gráficos
 combined_chart = area_chart + line_chart
-
-# Configurar el ancho del gráfico
-combined_chart = combined_chart.configure_view(
-    width=1200
-)
 
 # Mostrar el gráfico combinado en Streamlit
 st.altair_chart(combined_chart, use_container_width=True)
