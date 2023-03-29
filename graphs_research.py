698705638331844b7c0d83709db0241a03f2d022
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-29 09:30:00 +0200

    Update graphs_research.py

--- graphs_research.py
+++ graphs_research.py
@@ -3,22 +3,27 @@ import pandas as pd
 import plotly.graph_objects as go
 
 def mostrar_grafico_area_suavizado():
-  # Cargar tus datos desde el archivo CSV
-  data = pd.read_csv("csv/dep_inv_prisma.txt")
+    # Cargar tus datos desde el archivo CSV
+    data = pd.read_csv("csv/dep_inv_prisma.txt")
 
-  # Calcular el número de publicaciones por año
-  publications_by_year = data.groupby(['Año']).size().reset_index(name='Número de Publicaciones')
+    # Calcular el número de publicaciones por año
+    publications_by_year = data.groupby(['Año']).size().reset_index(name='Número de Publicaciones')
 
-  # Crear el gráfico de área suavizado
-  area_chart = go.Figure()
+    # Crear el gráfico de área suavizado
+    area_chart = go.Figure()
 
-  area_chart.add_trace(go.Scatter(x=publications_by_year['Año'], y=publications_by_year['Número de Publicaciones'],
-                                  mode='lines', line_shape='spline', fill='tozeroy', line=dict(smoothing=0.5)))
+    area_chart.add_trace(go.Scatter(x=publications_by_year['Año'], y=publications_by_year['Número de Publicaciones'],
+                                    mode='lines', line_shape='spline', fill='tozeroy', line=dict(smoothing=0.5, color='blue'), fillcolor='rgba(0, 0, 255, 0.2)'))
 
-  area_chart.update_layout(title='Evolución del número de publicaciones por año [2000-2022]',
-                           caption='Fuente: PRISMA, Universidad de Sevilla',
-                           xaxis=dict(range=[2000, 2022]), 
-                           width = 650)
+    area_chart.update_layout(title='Evolución del número de publicaciones por año [2000-2022]',
+                             xaxis=dict(range=[2000, 2022]),
+                             width=650,
+                             annotations=[dict(xref='paper', yref='paper', x=0, y=-0.1,
+                                               xanchor='left', yanchor='bottom',
+                                               text='Fuente: PRISMA, Universidad de Sevilla',
+                                               font=dict(size=12),
+                                               showarrow=False)])
 
-  # Mostrar el gráfico en Streamlit
-  st.plotly_chart(area_chart)
+    # Mostrar el gráfico en Streamlit
+    st.plotly_chart(area_chart)
+
