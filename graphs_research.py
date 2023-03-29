59b203db019c5c15b2c96537b335cf83a90bb54f
Manuel J. Sánchez-Franco <majesus@us.es>
2023-03-29 07:25:49 +0200

    Update graphs_research.py

--- graphs_research.py
+++ graphs_research.py
@@ -3,11 +3,9 @@ import altair as alt
 
 # Cargar tus datos desde el archivo CSV
 data = pd.read_csv("csv/dep_inv_prisma.txt", sep="/")
-st.write(data.columns)
 
 # Calcular el número de publicaciones por año
 publications_by_year = data.groupby(['Año']).size().reset_index(name='Número de Publicaciones')
-
 
 # Crear el gráfico de área apilada
 area_chart = alt.Chart(publications_by_year).mark_area(
