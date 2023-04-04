e402b6786e92d5582ac75566adc1b6bc408a0559
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-04 19:59:03 +0200

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -23,9 +23,9 @@ def separador(color):
         f'<hr style="border: 1px solid {color};">', unsafe_allow_html=True
     )
 #------------------------------------------------#
-custom_title_black = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 40px; line-height: 1.5; color: #000000; font-weight: bold;"
 custom_title = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 25px; line-height: 1.5; color: #B30A1B; font-weight: bold;"
 custom_subtitle = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 18px; line-height: 1.5; color: #B30A1B;"
+custom_subtitle_black = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 18px; line-height: 1.5; color: #000000; font-weight: bold;"
 custom_style = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 14px; line-height: 1.5;"
 
 # Aplicar estilos CSS personalizados a toda la página
@@ -57,7 +57,7 @@ col1, col2 = st.beta_columns([1, 3])  # La primera columna tendrá un ancho prop
 col1.image(imagen, width=150)
 # Añadir título en la segunda columna
 col2.title("")
-st.markdown(f"<p style='{custom_title}'>Por __*Manuel J. Sánchez Franco*__, Universidad de Sevilla.</p>", unsafe_allow_html=True)
+st.markdown(f"<p style='{custom_subtitle_black}'>Por Manuel J. Sánchez Franco, Universidad de Sevilla.</p>", unsafe_allow_html=True)
 separador("#B30A1B")
 #------------------------------------------------#
 st.write("Crea tus propios prompts, y ajusta los parámetros para obtener respuestas precisas y relevantes. A continuación, te formulamos distintas preguntas para alinear tu prompt con tus deseos.")
