79c72d6fa2f922198e43fd59e011d714f92d17e5
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-05 14:03:03 +0200

    Update ademark_normativa.py

--- ademark_normativa.py
+++ ademark_normativa.py
@@ -1,28 +1,41 @@
 import streamlit as st
-import requests
 import pandas as pd
-import io
+import requests
+from bs4 import BeautifulSoup
 
-# Hacer la petición HTTP para obtener el HTML
-url = "https://www.us.es/centro/secretariageneral/normativa"
+# Define la URL de la página web que contiene la información que queremos extraer.
+url = 'https://www.us.es/laUS/secretaria-general/normativas'
+
+# Realiza una petición a la página web y obtiene el código HTML como respuesta.
 response = requests.get(url)
 
-# Cargar el HTML en BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+# Parsea el código HTML con BeautifulSoup y extrae la lista de normativas.
+soup = BeautifulSoup(response.content, 'html.parser')
+normativas_list = soup.select_one('dl.ckeditor-accordion')
 
-# Buscar la tabla y convertirla en un objeto DataFrame de Pandas
-table = soup.find("dl", {"class": "ckeditor-accordion"})
-df = pd.read_html(str(table))[0]
-df.columns = ["Normativa", "URL"]
+# Crea listas vacías para almacenar los datos de la tabla.
+normativas = []
+urls = []
+codigos = []
 
-# Crear un objeto de memoria para escribir el archivo CSV
-buffer = io.StringIO()
-df.to_csv(buffer, index=False)
+# Extrae la información de cada normativa de la lista y la añade a las listas.
+for item in normativas_list.select('dt'):
+    normativas.append(item.text.strip())
+    
+    link = item.find_next('a')
+    urls.append(link['href'])
+    
+    codigos.append("")
 
-# Descargar el archivo CSV
-b64 = base64.b64encode(buffer.getvalue().encode()).decode()
-href = f'<a href="data:file/csv;base64,{b64}" download="normativas.csv">Descargar archivo CSV</a>'
-st.markdown(href, unsafe_allow_html=True)
+# Crea un DataFrame con los datos extraídos.
+data = {'Normativa': normativas, 'URL': urls, 'COD': codigos}
+df = pd.DataFrame(data)
 
-# Mostrar la tabla en pantalla
+# Muestra la tabla en pantalla.
 st.write(df)
+
+# Agrega un enlace para descargar la tabla en formato CSV.
+csv = df.to_csv(index=False)
+b64 = base64.b64encode(csv.encode()).decode()
+href = f'<a href="data:file/csv;base64,{b64}" download="tabla_normativas.csv">Descargar tabla en CSV</a>'
+st.markdown(href, unsafe_allow_html=True)
