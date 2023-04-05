cac6c022e116b99f1ebe0ba82fa1782725b9b881
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-05 14:06:45 +0200

    Update ademark_normativa.py

--- ademark_normativa.py
+++ ademark_normativa.py
@@ -3,46 +3,32 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# función para obtener el contenido de una url
-def get_url_content(url):
-    response = requests.get(url)
-    content = response.content
-    return content
+# Realizamos la petición GET a la URL y obtenemos el contenido
+url = "https://www.us.es/laUS/secretaria-general/normativas"
+res = requests.get(url)
+soup = BeautifulSoup(res.content, "html.parser")
 
-# url de la página a hacer scraping
-url = 'https://www.us.es/laUS/secretaria-general/normativas'
+# Buscamos el elemento que contiene todas las normativas
+normativas_element = soup.find("dl", class_="ckeditor-accordion")
 
-# hacer scraping del contenido de la página
-soup = BeautifulSoup(get_url_content(url), 'html.parser')
+# Creamos una lista vacía para ir almacenando las normativas
+normativas = []
 
-# encontrar el elemento de la página que contiene la lista de normativas
-lista_normativas = soup.find('dl', class_='ckeditor-accordion')
+# Iteramos por cada normativa y extraemos su nombre, URL y código
+for normativa in normativas_element.find_all("a"):
+    nombre = normativa.text
+    url = normativa.get("href")
+    cod = url.split("/")[-1]
+    normativas.append({"Normativa": nombre, "URL": url, "COD": cod})
 
-# inicializar una lista vacía para almacenar las filas de la tabla
-rows = []
+# Creamos un dataframe con las normativas
+df = pd.DataFrame(normativas)
 
-# iterar sobre cada elemento de la lista de normativas
-for normativa in lista_normativas.find_all('a'):
-
-    # obtener el título de la normativa y la url
-    title = normativa.text.strip()
-    url = normativa['href']
-    
-    # obtener el código de la normativa a partir de la url
-    cod = url.split('/')[-1]
-    
-    # agregar una nueva fila a la tabla
-    rows.append({'Normativa': title, 'URL': url, 'COD': cod})
-
-# convertir la lista de filas en un DataFrame de pandas
-df = pd.DataFrame(rows)
-
-# mostrar la tabla en pantalla
+# Mostramos la tabla en pantalla
 st.dataframe(df)
 
-# agregar un enlace para descargar la tabla en formato CSV
-csv = df.to_csv(index=False)
-b64 = base64.b64encode(csv.encode()).decode()
-href = f'<a href="data:file/csv;base64,{b64}" download="tabla_normativas.csv">Descargar CSV</a>'
-st.markdown(href, unsafe_allow_html=True)
-
+# Añadimos un enlace para descargar la tabla en formato CSV
+st.markdown(
+    f'<a href="data:application/octet-stream,{df.to_csv(index=False)}" download="normativas.csv">Descargar como CSV</a>',
+    unsafe_allow_html=True,
+)
