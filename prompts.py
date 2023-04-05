f914b86e1a43c3e1053de004718e30ee4f9f0020
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-05 11:06:50 +0200

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -25,7 +25,7 @@ def separador(color):
 #------------------------------------------------#
 custom_title = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 25px; line-height: 1.5; color: #B30A1B; font-weight: bold;"
 custom_subtitle = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 18px; line-height: 1.5; color: #B30A1B;"
-custom_subtitle_black = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 18px; line-height: 1.5; color: #000000; font-weight: bold;"
+custom_sub_subtitle_black = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 16px; line-height: 1.5;"
 custom_style = "font-family: Bahnschrift Light, Segoe UI, Arial; font-size: 14px; line-height: 1.5;"
 
 # Aplicar estilos CSS personalizados a toda la página
@@ -60,7 +60,7 @@ col2.title("")
 st.markdown(f"<p style='{custom_subtitle_black}'>Por Manuel J. Sánchez Franco, Universidad de Sevilla.</p>", unsafe_allow_html=True)
 separador("#B30A1B")
 #------------------------------------------------#
-st.markdown(f"<p style='{custom_subtitle_black}'>Crea tus propios prompts, y ajusta los parámetros para obtener respuestas precisas y relevantes. A continuación, te formulamos distintas preguntas para alinear tu prompt con tus deseos.</p>", unsafe_allow_html=True)
+st.markdown(f"<p style='{custom_sub_subtitle_black}'>Crea tus propios prompts, y ajusta los parámetros para obtener respuestas precisas y relevantes. A continuación, te formulamos distintas preguntas para alinear tu prompt con tus deseos.</p>", unsafe_allow_html=True)
 separador("#B30A1B")
 #------------------------------------------------#
 # Comienza el prompt y elementos adicionales
