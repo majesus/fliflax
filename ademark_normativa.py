37d7ca69d7990e57c1846b589f71617e181f1b27
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-05 15:23:46 +0200

    Update ademark_normativa.py

--- ademark_normativa.py
+++ ademark_normativa.py
@@ -17,7 +17,7 @@ for elem in elements:
     normativa_url = elem["href"]
     data.append([normativa, normativa_url, ""])
 
-df = pd.DataFrame(data, columns=["Normativa", "URL", "Columna Adicional"])
+df = pd.DataFrame(data, columns=["Normativa", "URL", "Codigo"])
 
 st.write(df)
 
@@ -26,3 +26,25 @@ st.download_button(label="Descargar CSV", data=csv, file_name="normativas.csv", 
 
 data = pd.read_csv("csv/normativas.csv") #path folder of the data file
 st.write(data) #displays the table of data
+#-------------------------------------------------------#
+# Crear el vector de valores proporcionados
+vector_valores = [
+    'P', 'E', 'PE', 'PE', 'E', 'PE', 'PE', 'E', 'E', 'E', 'PE', 'E', 'PE',
+    'E', 'PE', 'PE', 'E', 'PE', 'E', 'PE', 'PE', 'P', 'PE', 'P', 'E', 'PE',
+    'PE', 'PE', 'PE', 'E', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE',
+    'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'E', 'P', 'P',
+    'P', 'P', 'PE', 'P', 'P', 'P', 'P', 'PE', 'PE', 'P', 'P', 'P', 'P', 'PE',
+    'PE', 'PE', 'PE', 'PE', 'P', 'E', 'E', 'E', 'PE', 'PE', 'PE', 'PE', 'PE',
+    'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'PE', 'E'
+]
+
+# Asegurarse de que el vector tenga la misma longitud que el número de filas en el DataFrame
+assert len(vector_valores) == len(df)
+
+# Agregar la columna con el vector de valores al DataFrame
+df['Codigo'] = vector_valores
+st.write(df)
+
+# Guardar el DataFrame actualizado en un nuevo archivo CSV
+csv = df.to_csv(index=False, encoding="utf-8")
+st.download_button(label="Descargar CSV", data=csv, file_name="normativas.csv", mime="text/csv")
