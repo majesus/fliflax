86a6548a119e86ad1fbe3b5871527d3f7dfd0641
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-10 13:08:01 +0200

    Update ademark_KG.py

--- ademark_KG.py
+++ ademark_KG.py
@@ -1,124 +1,121 @@
-import streamlit as st
-import networkx as nx
-import plotly.graph_objects as go
+def knowledge_graph_app():
 
-def create_knowledge_graph(edges):
-    G = nx.Graph()
-    G.add_edges_from(edges)
+    import streamlit as st
+    import networkx as nx
+    import plotly.graph_objects as go
 
-    pos = nx.spring_layout(G, seed=42)
+    def create_knowledge_graph(edges):
+        G = nx.Graph()
+        G.add_edges_from(edges)
 
-    edge_x = []
-    edge_y = []
-    for edge in G.edges():
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
-        edge_x.extend([x0, x1, None])
-        edge_y.extend([y0, y1, None])
+        pos = nx.spring_layout(G, seed=42)
 
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=0.5, color="#888"),
-        hoverinfo="none",
-        mode="lines")
+        edge_x = []
+        edge_y = []
+        for edge in G.edges():
+            x0, y0 = pos[edge[0]]
+            x1, y1 = pos[edge[1]]
+            edge_x.extend([x0, x1, None])
+            edge_y.extend([y0, y1, None])
 
-    node_x = [pos[node][0] for node in G.nodes()]
-    node_y = [pos[node][1] for node in G.nodes()]
+        edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color="#888"),
+            hoverinfo="none",
+            mode="lines")
 
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode="markers",
-        hoverinfo="text",
-        marker=dict(
-            showscale=True,
-            colorscale="YlGnBu",
-            reversescale=True,
-            color=[],
-            size=10,
-            colorbar=dict(
-                thickness=15,
-                title="Node Connections",
-                xanchor="left",
-                titleside="right"),
-            line_width=2))
-    
-    offset = -0.05
-    node_label_x = [pos[node][0] + offset for node in G.nodes()]
-    node_label_y = [pos[node][1] + offset for node in G.nodes()]
+        node_x = [pos[node][0] for node in G.nodes()]
+        node_y = [pos[node][1] for node in G.nodes()]
 
-    node_label_trace = go.Scatter(
-        x=node_label_x, y=node_label_y,
-        mode="text",
-        hoverinfo="none",
-        text=[node for node in G.nodes()],
-        textposition="bottom center",
-        textfont=dict(size=12, color="#000"))
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode="markers",
+            hoverinfo="text",
+            marker=dict(
+                showscale=True,
+                colorscale="YlGnBu",
+                reversescale=True,
+                color=[],
+                size=10,
+                colorbar=dict(
+                    thickness=15,
+                    title="Node Connections",
+                    xanchor="left",
+                    titleside="right"),
+                line_width=2))
 
-    node_adjacencies = []
-    node_text = []
-    for node, adjacencies in G.adjacency():
-        node_adjacencies.append(len(adjacencies))
-        node_text.append(f"{node} - # of connections: {len(adjacencies)}")
+        offset = -0.05
+        node_label_x = [pos[node][0] + offset for node in G.nodes()]
+        node_label_y = [pos[node][1] + offset for node in G.nodes()]
 
-    node_trace.marker.color = node_adjacencies
-    node_trace.text = node_text
+        node_label_trace = go.Scatter(
+            x=node_label_x, y=node_label_y,
+            mode="text",
+            hoverinfo="none",
+            text=[node for node in G.nodes()],
+            textposition="bottom center",
+            textfont=dict(size=12, color="#000"))
 
-    fig = go.Figure(data=[edge_trace, node_trace, node_label_trace],
-                    layout=go.Layout(
-                        title="Knowledge Graph",
-                        titlefont=dict(size=16),
-                        showlegend=False,
-                        hovermode="closest",
-                        margin=dict(b=20, l=5, r=5, t=40),
-                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-                    )
-    return fig
+        node_adjacencies = []
+        node_text = []
+        for node, adjacencies in G.adjacency():
+            node_adjacencies.append(len(adjacencies))
+            node_text.append(f"{node} - # of connections: {len(adjacencies)}")
 
-edges = [
-    ("Marketing", "Digital Marketing"),
-    ("Marketing", "Content Marketing"),
-    ("Marketing", "Customer Relationship Management"),
-    ("Digital Marketing", "SEO"),
-    ("Digital Marketing", "SEM"),
-    ("Digital Marketing", "Social Media Marketing"),
-    ("Content Marketing", "Influencer Marketing"),
-    ("Content Marketing", "Video Marketing"),
-    ("Customer Relationship Management", "Customer Retention"),
-    ("Customer Relationship Management", "Customer Acquisition"),
-    ("SEO", "On-page SEO"),
-    ("SEO", "Off-page SEO"),
-    ("SEM", "Google Ads"),
-    ("SEM", "Facebook Ads"),
-    ("Social Media Marketing", "Instagram Marketing"),
-    ("Social Media Marketing", "LinkedIn Marketing"),
-    ("Organizational Behavior", "Employee Motivation"),
-    ("Organizational Behavior", "Teamwork"),
-    ("Organizational Behavior", "Leadership"),
-    ("Employee Motivation", "Job Satisfaction"),
-    ("Employee Motivation", "Performance Management"),
-    ("Teamwork", "Communication"),
-    ("Teamwork", "Conflict Resolution"),
-    ("Leadership", "Transformational Leadership"),
-    ("Leadership", "Servant Leadership"),
-    ("Entrepreneurship", "Startup Success"),
-    ("Entrepreneurship", "Intrapreneurship"),
-    ("Startup Success", "Innovation"),
-    ("Startup Success", "Market Validation"),
-    ("Intrapreneurship", "Employee Innovation"),
-    # Añadiendo conexiones para interconectar áreas relevantes
-    ("Marketing", "Entrepreneurship"),
-    ("Organizational Behavior", "Entrepreneurship"),
-    ("Digital Marketing", "Innovation"),
-    ("Leadership", "Innovation"),
-    ]
+        node_trace.marker.color = node_adjacencies
+        node_trace.text = node_text
 
+        fig = go.Figure(data=[edge_trace, node_trace, node_label_trace],
+                        layout=go.Layout(
+                            title="Knowledge Graph",
+                            titlefont=dict(size=16),
+                            showlegend=False,
+                            hovermode="closest",
+                            margin=dict(b=20, l=5, r=5, t=40),
+                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )
+        return fig
 
+    edges = [
+        ("Marketing", "Digital Marketing"),
+        ("Marketing", "Content Marketing"),
+        ("Marketing", "Customer Relationship Management"),
+        ("Digital Marketing", "SEO"),
+        ("Digital Marketing", "SEM"),
+        ("Digital Marketing", "Social Media Marketing"),
+        ("Content Marketing", "Influencer Marketing"),
+        ("Content Marketing", "Video Marketing"),
+        ("Customer Relationship Management", "Customer Retention"),
+        ("Customer Relationship Management", "Customer Acquisition"),
+        ("SEO", "On-page SEO"),
+        ("SEO", "Off-page SEO"),
+        ("SEM", "Google Ads"),
+        ("SEM", "Facebook Ads"),
+        ("Social Media Marketing", "Instagram Marketing"),
+        ("Social Media Marketing", "LinkedIn Marketing"),
+        ("Organizational Behavior", "Employee Motivation"),
+        ("Organizational Behavior", "Teamwork"),
+        ("Organizational Behavior", "Leadership"),
+        ("Employee Motivation", "Job Satisfaction"),
+        ("Employee Motivation", "Performance Management"),
+        ("Teamwork", "Communication"),
+        ("Teamwork", "Conflict Resolution"),
+        ("Leadership", "Transformational Leadership"),
+        ("Leadership", "Servant Leadership"),
+        ("Entrepreneurship", "Startup Success"),
+        ("Entrepreneurship", "Intrapreneurship"),
+        ("Startup Success", "Innovation"),
+        ("Startup Success", "Market Validation"),
+        ("Intrapreneurship", "Employee Innovation"),
+        # Añadiendo conexiones para interconectar áreas relevantes
+        ("Marketing", "Entrepreneurship"),
+        ("Organizational Behavior", "Entrepreneurship"),
+        ("Digital Marketing", "Innovation"),
+        ("Leadership", "Innovation"),
+        ]
 
+    st.title("Knowledge Graph")
 
-
-st.title("Knowledge Graph")
-
-fig = create_knowledge_graph(edges)
-st.plotly_chart(fig)
-
+    fig = create_knowledge_graph(edges)
+    st.plotly_chart(fig)
