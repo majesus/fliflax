640cd90b525c84dd7ba4a962c4432ac488c63bfb
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-10 11:59:01 +0200

    Create ademark_KG.py

---
+++ ademark_KG.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import networkx as nx
+import plotly.graph_objects as go
+
+def create_knowledge_graph(edges):
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    pos = nx.spring_layout(G, seed=42)
+
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.extend([x0, x1, None])
+        edge_y.extend([y0, y1, None])
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color="#888"),
+        hoverinfo="none",
+        mode="lines")
+
+    node_x = [pos[node][0] for node in G.nodes()]
+    node_y = [pos[node][1] for node in G.nodes()]
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode="markers",
+        hoverinfo="text",
+        marker=dict(
+            showscale=True,
+            colorscale="YlGnBu",
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title="Node Connections",
+                xanchor="left",
+                titleside="right"),
+            line_width=2))
+
+    for node, adjacencies in enumerate(G.adjacency()):
+        node_trace.marker.color.append(len(adjacencies[1]))
+        node_info = f"{adjacencies[0]} - # of connections: {len(adjacencies[1])}"
+        node_trace.text.append(node_info)
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                        title="Knowledge Graph",
+                        titlefont=dict(size=16),
+                        showlegend=False,
+                        hovermode="closest",
+                        margin=dict(b=20, l=5, r=5, t=40),
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
+    return fig
+
+  
+edges = [("Marketing", "SEO"),
+         ("Marketing", "SEM"),
+         ("Marketing", "Email Marketing"),
+         ("SEO", "On-page"),
+         ("SEO", "Off-page"),
+         ("SEM", "Google Ads"),
+         ("SEM", "Bing Ads"),
+         ("Email Marketing", "Newsletter"),
+         ("Email Marketing", "Drip Campaign")]
+
+st.title("Marketing Knowledge Graph")
+
+fig = create_knowledge_graph(edges)
+st.plotly_chart(fig)
+
