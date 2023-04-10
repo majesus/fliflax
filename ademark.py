d2d6a6c2522af00652c5a6089d07381f2212f648
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-11 00:33:38 +0200

    Update ademark.py

--- ademark.py
+++ ademark.py
@@ -182,7 +182,7 @@ elif menu == "Estudiantes":
         st.markdown(f"<p style='{custom_style}', color: #B30A1B>AVISO: En cada centro puedes consultar la normativa específica de los TFE.</p>", unsafe_allow_html=True)
     #---------------------------
     # Asegúrate de cambiar la ruta del archivo a la ubicación de tu archivo PDF
-    with open("csv/Tutorias.xlsb", "rb") as pdf_file: # img/tutorias_cuarta_planta_fceye.pdf
+    with open("csv/Tutorías.xlsb", "rb") as pdf_file: # img/tutorias_cuarta_planta_fceye.pdf
         PDFbyte = pdf_file.read()
     st.download_button(label="Descargar PDF", 
             data=PDFbyte,
