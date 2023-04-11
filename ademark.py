e351f7663bcc207b7890387bb6222bd6b90aa29a
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-11 22:22:26 +0200

    Update ademark.py

--- ademark.py
+++ ademark.py
@@ -256,7 +256,7 @@ elif menu == "Investigación":
     # Selector de profesores
     df_result = df_result0.set_index('Nombre')
     #st.markdown(f"<p style='{custom_style}'>Selecciona su nombre:</p>", unsafe_allow_html=True)
-    selected_indices = st.selectbox("'Selecciona su nombre'", options = df_result.index.unique(), key="custom_selectbox_2", label_visibility = "collapsed")
+    selected_indices = st.selectbox('Selecciona su nombre', df_result.index.unique(), label_visibility = "collapsed")
     #selected_indices = st.multiselect('Selecciona su nombre:', df_result.index.unique(), help='Elige un profesor de la lista')
         
     import re
