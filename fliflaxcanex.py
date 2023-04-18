12e0e325c1cf9981e738d267623006dd0ced37aa
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-18 22:44:12 +0200

    Update fliflaxcanex.py

--- fliflaxcanex.py
+++ fliflaxcanex.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 
 # Función para generar datos de exposición basados en inserciones
-def generate_exposure_data(n, p, inserciones):
-    data = np.zeros((n, len(p)))
-    for i in range(len(p)):
+def generate_cumulative_exposure(n, exposure_ratios, inserciones):
+    data = np.zeros((n, len(exposure_ratios)))
+    for i in range(len(exposure_ratios)):
         for j in range(inserciones[i]):
-            random_data = np.random.binomial(1, p[i], n)
+            random_data = np.random.binomial(1, exposure_ratios[i], n)
             data[:, i] = np.logical_or(data[:, i], random_data).astype(int)
     return data
+
 
 # Función iterativa para calcular la suma de productos M-duples de covarianzas y probabilidades marginales
 def calc_mduple_sum_iterative(cov_matrix, p):
