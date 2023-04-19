0e9d072cb066301098735548e560ab4b9136e65f
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-19 17:27:41 +0200

    Update fliflax_meth.py

--- fliflax_meth.py
+++ fliflax_meth.py
@@ -94,7 +94,6 @@ except ZeroDivisionError as e:
            "Debes pues revisar los valores de A1 y A2. "
            "Mientras tanto, los resultados que ves abajo, se corresponden con valores por defecto.")
 #----------------------------------------------------#
-n = inserciones
 x = np.arange(1,n+1)
 alphas = alpha
 betas = beta
