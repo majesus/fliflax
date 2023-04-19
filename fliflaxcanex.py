02bc52c58b588eb86e2e1f57e388ff57615be4a2
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-19 10:32:41 +0200

    Update fliflaxcanex.py

--- fliflaxcanex.py
+++ fliflaxcanex.py
@@ -33,7 +33,7 @@ st.set_page_config(page_title="Planificación de Medios", page_icon="📊", layo
 st.title("Planificación de Medios")
 st.write("Cálculo del alcance del plan de medios y la distribución de contactos utilizando el modelo CANEX")
 
-#------------------------------------------#
+# Crear un DataFrame ficticio
 np.random.seed(42)  # Fijar una semilla para obtener resultados consistentes
 
 data_ficticia = pd.DataFrame({
@@ -42,16 +42,18 @@ data_ficticia = pd.DataFrame({
     'Medio3': np.random.choice([0, 1], size=100, p=[0.5, 0.5])
 })
 
-# if uploaded_file is not None:
-#     data = pd.read_csv(uploaded_file)
-data = data_ficticia
+data = data_ficticia.copy()
 marginales = data.mean()
 primer_orden = data.corr()
 segundo_orden = calcular_segundo_orden(data)
-#------------------------------------------#
 
-alcance = calcular_alcance(marginales, inserciones)
-distribucion_contactos = calcular_distribucion_contactos(marginales, inserciones)
+# Solicitar el número de inserciones por medio
+inserciones = {}
+for medio in data.columns:
+    inserciones[medio] = st.sidebar.number_input(f"Inserciones en {medio}", value=1, min_value=1, step=1)
+
+alcance = calcular_alcance(marginales, pd.Series(inserciones))
+distribucion_contactos = calcular_distribucion_contactos(marginales, pd.Series(inserciones))
 st.subheader("Alcance del plan de medios")
 st.write(alcance)
 st.subheader("Distribución de contactos")
