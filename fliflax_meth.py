4a15ed3575cf2eee190072d99615656793e19978
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-19 17:10:39 +0200

    Update fliflax_meth.py

--- fliflax_meth.py
+++ fliflax_meth.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy.stats import betabinom
+import matplotlib.pyplot as plt
+from scipy import special
 
 # 1) Sliders para Ai, ni y P
 st.sidebar.header("Configuración de parámetros")
@@ -15,26 +16,28 @@ for i in range(M):
     A_list.append(st.sidebar.slider(f"Audiencia del Medio {i+1} (A{i+1})", 0, 100000, 10000))
     n_list.append(st.sidebar.slider(f"Inserciones en el Medio {i+1} (n{i+1})", 0, 100, 10))
 
-P = st.sidebar.slider("Población (P)", 0, 1000000, 100000)
+# Población superior a la mayor de las audiencias
+max_audience = max(A_list)
+P = st.sidebar.number_input("Población (P)", value=max_audience+1, min_value=max_audience+1)
 
 # 2) Tabla de duplicaciones del Medio i con i, y el Medio i con j
-st.header("Duplicaciones")
-duplication_df = pd.DataFrame(index=range(M), columns=range(M))
-duplication_input = []
+with st.expander("Duplicaciones"):
+    duplication_df = pd.DataFrame(index=range(M), columns=range(M))
+    duplication_input = []
 
-for i in range(M):
-    for j in range(i, M):
-        duplication_input.append(st.number_input(f"Duplicación del Medio {i+1} con {j+1}", key=f"{i},{j}"))
+    for i in range(M):
+        for j in range(i, M):
+            duplication_input.append(st.number_input(f"Duplicación del Medio {i+1} con {j+1}", key=f"{i},{j}"))
 
-duplication_input = iter(duplication_input)
+    duplication_input = iter(duplication_input)
 
-for i in range(M):
-    for j in range(i, M):
-        value = next(duplication_input)
-        duplication_df.at[i, j] = value
-        duplication_df.at[j, i] = value
+    for i in range(M):
+        for j in range(i, M):
+            value = next(duplication_input)
+            duplication_df.at[i, j] = value
+            duplication_df.at[j, i] = value
 
-st.write(duplication_df)
+    st.write(duplication_df)
 
 # 3) Matriz de opciones de duplicación i con i, e i con j
 options_df = pd.DataFrame(index=range(M), columns=range(M))
@@ -60,43 +63,33 @@ D = np.triu(multiplication_df).sum() / np.triu(options_df).sum()
 
 # 7) Cálculo de C2
 C2 = 2 * C1 - D
-#----------------------------------------------------#
-#----------------------------------------------------#
-R1=C1/P;R2=C2/P  
-alpha=((R1)*((R2)-(R1)))/(2*(R1)-(R1)*(R1)-(R2))
-beta=(alpha*(1-R1))/(R1)
-#----------------------------------------------------#
+
+# Valores redondeados
+C1_rounded = round(C1, 4)
+D_rounded = round(D, 4)
+C2_rounded = round(C2, 4)
+
+# Cálculo de alphas y betas para la distribución Beta Binomial
+R1 = C1 / P
+R2 = C2 / P
+
 try:
-  alpha=((R1)*((R2)-(R1)))/(2*(R1)-(R1)*(R1)-(R2))
-  beta=(alpha*(1-R1))/(R1)
+    alpha = (R1 * (R2 - R1)) / (2 * R1 - R1 * R1 - R2)
+    beta = (alpha * (1 - R1)) / R1
 except ZeroDivisionError as e:
-  # st.write("#### Observaciones:")
-  # datos de muestra:
-  alpha = 0.125
-  beta = 0.125
-  n = 5
-  st.error("Se ha producido una excepción al proponerse un valor de C2 que provoca una división por 0. "
-           "Recuerda que los parámetros de forma deben ser superiores a 0 ."
-           "Debes pues revisar los valores de A1 y A2. "
-           "Mientras tanto, los resultados que ves abajo, se corresponden con valores por defecto.")
-#----------------------------------------------------#
-n = sum(n_list)
-st.write(n)
+    alpha = 0.125
+    beta = 0.125
+    st.error("Se ha producido una excepción al proponerse un valor de C2 que provoca una división por 0. "
 
-x = np.arange(1,n+1)
-st.write(x)
-alphas = alpha
-betas = beta
-#----------------------------------------------------#
-# https://docs.pymc.io/en/v3/api/distributions/discrete-2.py
-# https://docs.scipy.org/doc/scipy/tutorial/stats/discrete_betabinom.html
+         
 def BetaBinom(a, b, n, x):
-    pmf = special.binom(n, x) * (special.beta(x+a, n-x+b) / special.beta(a, b))
-    return pmf
+         pmf = special.binom(n, x) * (special.beta(x+a, n-x+b) / special.beta(a, b))
+         return pmf
+         
+         
+n = sum(n_list)
+x = np.arange(1, n+1)
 
-# eliminar primer elemento de la lista pmf que hace referencia a los individuos no expuestos:
-dc = BetaBinom(alphas, betas, n, x); 
-  
 if alphas > 0 and betas > 0 and P > C2:
   pmf = BetaBinom(alphas, betas, n, x)
 else:
@@ -112,10 +105,20 @@ else:
   P = 1000000
   alphas = 0.125
   betas = 0.125
-  pmf = BetaBinom(alphas, betas, n, x)
-  
-# Pi:
-y = pmf * P
-# Ri:
-Ri = np.flip(y); Ri = np.cumsum(Ri); Ri = np.flip(Ri)
+  pmf = BetaBinom(alphas, betas, n, x)  
+             
+Ri = np.flip(y)
+Ri = np.cumsum(Ri)
+Ri = np.flip(Ri)
+             
+freq_population_pct = y / P * 100
+freq_population_pct = y / P * 100       
+freq_people = y
 
+contacts_df = pd.DataFrame({"% Población": freq_population_pct,
+"% Cobertura": freq_coverage_pct,
+"Personas": freq_people},
+index=range(n+1))
+contacts_df.index.name = "Contactos"
+
+st.write(contacts_df)
