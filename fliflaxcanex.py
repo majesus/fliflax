df01a9e19768bdb67735f4f569ac6ea191140deb
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-19 11:15:37 +0200

    Update fliflaxcanex.py

--- fliflaxcanex.py
+++ fliflaxcanex.py
@@ -7,53 +7,36 @@ def calcular_prob_conjunta(data, marginales):
     n = len(data.columns)
     medios = data.columns
     prob_conjunta = pd.DataFrame(columns=medios, index=medios)
-    
+
     for medio_i, medio_j in itertools.combinations(medios, 2):
         prob_conjunta.at[medio_i, medio_j] = marginales[medio_i] * marginales[medio_j]
         prob_conjunta.at[medio_j, medio_i] = prob_conjunta.at[medio_i, medio_j]
-        
+
     for medio in medios:
         prob_conjunta.at[medio, medio] = marginales[medio]
-        
+
     return prob_conjunta
 
 def calcular_alcance(marginales, prob_conjunta):
     return 1 - np.prod(1 - marginales + np.diag(prob_conjunta))
 
 def calcular_distribucion_contactos(marginales, inserciones, alcance):
-    distribucion = pd.DataFrame(index=range(1, inserciones.sum() + 1), columns=['Contactos'])
-    for i in range(1, inserciones.sum() + 1):
-        distribucion.loc[i, 'Contactos'] = np.sum(marginales ** i) / alcance
+    distribucion = pd.DataFrame(index=range(1, sum(inserciones.values()) + 1), columns=['Contactos'])
+    for i in range(1, sum(inserciones.values()) + 1):
+        distribucion.loc[i, 'Contactos'] = np.sum(marginales * i) / alcance
 
     return distribucion
 
-st.set_page_config(page_title="Planificación de Medios", page_icon="📊", layout="centered")
-
-st.title("Planificación de Medios")
-st.write("Cálculo del alcance del plan de medios y la distribución de contactos utilizando el modelo CANEX")
-
+# Simulación de datos de exposición
 np.random.seed(42)
-
-# Sidebar
-st.sidebar.title("Parámetros del Plan de Medios")
-poblacion = st.sidebar.number_input("Población", value=1000, min_value=1)
-audiencias = {
-    'Medio1': st.sidebar.number_input("Audiencia Medio1", value=400, min_value=0, max_value=poblacion),
-    'Medio2': st.sidebar.number_input("Audiencia Medio2", value=300, min_value=0, max_value=poblacion),
-    'Medio3': st.sidebar.number_input("Audiencia Medio3", value=500, min_value=0, max_value=poblacion)
-}
-inserciones = {
-    'Medio1': st.sidebar.number_input("Inserciones Medio1", value=1, min_value=0),
-    'Medio2': st.sidebar.number_input("Inserciones Medio2", value=1, min_value=0),
-    'Medio3': st.sidebar.number_input("Inserciones Medio3", value=1, min_value=0)
-}
-
-data = pd.DataFrame(audiencias, index=[0]).div(poblacion)
+data = pd.DataFrame({'Medio1': np.random.binomial(1, 400 / poblacion, poblacion),
+                     'Medio2': np.random.binomial(1, 300 / poblacion, poblacion),
+                     'Medio3': np.random.binomial(1, 500 / poblacion, poblacion)})
 
 marginales = data.mean()
 prob_conjunta = calcular_prob_conjunta(data, marginales)
 alcance = calcular_alcance(marginales, prob_conjunta)
-distribucion_contactos = calcular_distribucion_contactos(marginales, pd.Series(inserciones), alcance)
+distribucion_contactos = calcular_distribucion_contactos(marginales, inserciones, alcance)
 
 st.subheader("Probabilidades marginales")
 st.write(marginales)
