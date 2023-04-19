bc5ba15029287c1c1d7919918b6fd5394edf3994
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-19 11:01:33 +0200

    Update fliflaxcanex.py

--- fliflaxcanex.py
+++ fliflaxcanex.py
@@ -3,27 +3,26 @@ import itertools
 import streamlit as st
 import numpy as np
 
-def calcular_prob_conjunta(data, marginales):
+def calcular_prob_conjunta(data):
+    n = len(data)
     medios = data.columns
     prob_conjunta = pd.DataFrame(columns=medios, index=medios)
     
     for medio_i, medio_j in itertools.combinations(medios, 2):
-        prob_conjunta.at[medio_i, medio_j] = data[(data[medio_i] == 1) & (data[medio_j] == 1)].shape[0] / len(data)
+        prob_conjunta.at[medio_i, medio_j] = data[(data[medio_i] == 1) & (data[medio_j] == 1)].shape[0] / n
         prob_conjunta.at[medio_j, medio_i] = prob_conjunta.at[medio_i, medio_j]
-    
-    for medio_i in medios:
-        prob_conjunta.at[medio_i, medio_i] = marginales[medio_i]
-    
+        
     return prob_conjunta
 
-def calcular_alcance(prob_conjunta):
-    alcance = 1 - prob_conjunta.at['Ninguno', 'Ninguno']
-    return alcance
+def calcular_alcance(marginales, prob_conjunta):
+    return 1 - np.prod(1 - marginales + np.diag(prob_conjunta))
 
-def calcular_distribucion_contactos(prob_conjunta):
-    medios = prob_conjunta.columns[:-1]  # Excluir la columna 'Ninguno'
-    distribucion_contactos = prob_conjunta.loc[medios, 'Ninguno'] * -1
-    return distribucion_contactos
+def calcular_distribucion_contactos(data, inserciones, alcance):
+    distribucion = pd.DataFrame(index=range(1, inserciones.sum() + 1), columns=['Contactos'])
+    for i in range(1, inserciones.sum() + 1):
+        distribucion.loc[i, 'Contactos'] = np.sum(data ** i) / alcance
+
+    return distribucion
 
 st.set_page_config(page_title="Planificación de Medios", page_icon="📊", layout="centered")
 
@@ -32,34 +31,26 @@ st.write("Cálculo del alcance del plan de medios y la distribución de contacto
 
 np.random.seed(42)
 
-data_ficticia = pd.DataFrame({
-    'Medio1': np.random.choice([0, 1], size=100, p=[0.6, 0.4]),
-    'Medio2': np.random.choice([0, 1], size=100, p=[0.7, 0.3]),
-    'Medio3': np.random.choice([0, 1], size=100, p=[0.5, 0.5])
-})
-
-data = data_ficticia.copy()
-
-# Agregar una columna 'Ninguno' para calcular el alcance y la distribución de contactos
-data['Ninguno'] = (~data.any(axis=1)).astype(int)
-
-# Sidebar para ingresar datos
-st.sidebar.title("Ingrese los datos del plan de medios")
+# Sidebar
+st.sidebar.title("Parámetros del Plan de Medios")
+poblacion = st.sidebar.number_input("Población", value=1000, min_value=1)
+audiencias = {
+    'Medio1': st.sidebar.number_input("Audiencia Medio1", value=400, min_value=0, max_value=poblacion),
+    'Medio2': st.sidebar.number_input("Audiencia Medio2", value=300, min_value=0, max_value=poblacion),
+    'Medio3': st.sidebar.number_input("Audiencia Medio3", value=500, min_value=0, max_value=poblacion)
+}
 inserciones = {
-    medio: st.sidebar.number_input(f"Inserciones en {medio}", min_value=0, value=1)
-    for medio in data.columns[:-1]
+    'Medio1': st.sidebar.number_input("Inserciones Medio1", value=1, min_value=0),
+    'Medio2': st.sidebar.number_input("Inserciones Medio2", value=1, min_value=0),
+    'Medio3': st.sidebar.number_input("Inserciones Medio3", value=1, min_value=0)
 }
 
-poblacion = st.sidebar.number_input("Población", min_value=1, value=100)
-
-# Aplicar las inserciones en el plan de medios
-for medio, num_inserciones in inserciones.items():
-    data.loc[data[medio] == 1, medio] = num_inserciones
+data = pd.DataFrame(audiencias, index=[0]).div(poblacion)
 
 marginales = data.mean()
-prob_conjunta = calcular_prob_conjunta(data, marginales)
-alcance = calcular_alcance(prob_conjunta)
-distribucion_contactos = calcular_distribucion_contactos(prob_conjunta)
+prob_conjunta = calcular_prob_conjunta(data)
+alcance = calcular_alcance(marginales, prob_conjunta)
+distribucion_contactos = calcular_distribucion_contactos(data, pd.Series(inserciones), alcance)
 
 st.subheader("Probabilidades marginales")
 st.write(marginales)
