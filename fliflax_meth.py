dc49012a39ef9c6fd6366189a3114160d4790c40
Manuel J. Sánchez-Franco <majesus@us.es>
2023-04-20 12:31:52 +0200

    Update fliflax_meth.py

--- fliflax_meth.py
+++ fliflax_meth.py
@@ -135,10 +135,10 @@ def update_correlation_matrix_with_Dii(correlation_matrix, data, P):
 
     return correlation_matrix
 
-#data = create_dataset(M, 150)
-correlation_matrix_0 = calculate_phi_correlation_matrix(data)
-min_audience_matrix = create_min_audience_matrix(A_list)
-correlation_matrix = adjust_correlation_matrix(correlation_matrix_0, min_audience_matrix)
+data = create_dataset(M, 150)
+#correlation_matrix_0 = calculate_phi_correlation_matrix(data)
+#min_audience_matrix = create_min_audience_matrix(A_list)
+#correlation_matrix = adjust_correlation_matrix(correlation_matrix_0, min_audience_matrix)
 
 P = 150 # Cambia esto por el valor real de la población
 correlation_matrix_with_Dii = update_correlation_matrix_with_Dii(correlation_matrix, data, P)
