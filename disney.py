7dd1555ee3ac2d76ecb591e18f670d04ef8ee2a1
Manuel J. Sánchez-Franco <majesus@us.es>
2023-05-16 21:00:56 +0200

    Update disney.py

--- disney.py
+++ disney.py
@@ -11,7 +11,11 @@ all_reviews = []
 
 for page in pages:
     st.write(f'Scraping page: {base_url + page + suffix}')
-    response = requests.get(base_url + page + suffix)
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(base_url + page + suffix, headers=headers)
+
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
